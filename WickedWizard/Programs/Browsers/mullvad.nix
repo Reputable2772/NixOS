ac0beda8c3f4412086c173c4e0c10308d11756ef
WickedWizard3588 <coding.exhibiting@slmails.com>
2023-09-29 12:02:26 +0530

    feat(autostart): autostart joplin, mullvad and newsflash

--- WickedWizard/Programs/Browsers/mullvad.nix
+++ WickedWizard/Programs/Browsers/mullvad.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [ mullvad-browser ];
 
-  xdg.configFile."autostart/mullvad-browser.desktop".source = "${pkgs.mullvad-browser}/share/applications/mullvad-browser.desktop";
+  xdg.configFile."autostart/mullvad-browser.desktop".source = "${pkgs.mullvad-browser}/share/applications/mullvadbrowser.desktop";
 }
--- WickedWizard/Programs/Productivity/joplin.nix
+++ WickedWizard/Programs/Productivity/joplin.nix
@@ -1,3 +1,5 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [ joplin-desktop ];
+
+  xdg.configFile."autostart/joplin-desktop.desktop".source = "${pkgs.joplin-desktop}/share/applications/@joplinapp-desktop.desktop";
 }
