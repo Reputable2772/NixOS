9a813018f085e2f65bd05fb427b3993fe19702d5
WickedWizard3588 <coding.exhibiting@slmails.com>
2023-09-29 14:58:21 +0530

    feat(diff): show diff between generations

--- nix.sh
+++ nix.sh
@@ -5,12 +5,12 @@ nix fmt
 nix flake check
 
 echo "Dumping DConf"
-dconf dump / | dconf2nix  --timeout 360 > ./WickedWizard/Programs/Desktop/Gnome/gnome.nix
-sudo -u shuba dconf dump / | dconf2nix --timeout 360 > ./Shuba/gnome.nix
+dconf dump / | dconf2nix > ./WickedWizard/Programs/Desktop/Gnome/gnome.nix
+sudo -u shuba dconf dump / | dconf2nix > ./Shuba/gnome.nix
 
 echo "Actually builiding"
 rm -rf /home/wickedwizard/Documents/Browsers/Firefox/search.json.mozlz4 /home/wickedwizard/.config/mimeapps.list
 sudo nixos-rebuild switch --flake .#nixos
 
 echo "Changelog"
-nix profile diff-closures -p /nix/var/nix/profiles/system
+nix profile diff-closures --profile /nix/var/nix/profiles/system
