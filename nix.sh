82ff781450406775bd9665c37a80f4531f6d79e7
WickedWizard3588 <coding.exhibiting@slmails.com>
2023-10-01 22:40:39 +0530

    fix(newline): parse and fix dconf2nix's newline issue

--- nix.sh
+++ nix.sh
@@ -1,12 +1,20 @@
 #!bash
 
+set -e
+
 echo "Formatting and checking files."
 nix fmt
 nix flake check
 
 echo "Dumping DConf"
-dconf dump / | dconf2nix > ./WickedWizard/Programs/Desktop/Gnome/gnome.nix
-sudo -u shuba dconf dump / | dconf2nix > ./Shuba/gnome.nix
+dconf dump / > wickedwizard.dconf
+sudo -u shuba dconf dump / > shuba.conf
+
+sed -e 's/\\n/\n/g' wickedwizard.dconf > wickedwizard.conf
+sed -e 's/\\n/\n/g' shuba.dconf > shuba.conf
+
+dconf2nix -i wickedwizard.conf -o ./WickedWizard/Programs/Desktop/Gnome/gnome.nix
+dconf2nix -i shuba.conf -o ./Shuba/gnome.nix
 
 echo "Actually builiding"
 rm -rf /home/wickedwizard/Documents/Browsers/Firefox/search.json.mozlz4 /home/wickedwizard/.config/mimeapps.list
