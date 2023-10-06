796682bf9b5fde02be1826beba7462ca501b3a70
WickedWizard3588 <coding.exhibiting@slmails.com>
2023-10-06 23:28:33 +0530

    fix(beeper): use beeper package from mjm

---
+++ Packages/beeper.nix
@@ -0,0 +1,61 @@
+# Taken from https://git.midna.dev/mjm/nix-config/-/blob/main/packages/beeper.nix
+
+{ appimageTools
+, lib
+, fetchurl
+, makeWrapper
+, writeShellScript
+, curl
+, gnugrep
+, pcre
+, common-updater-scripts
+,
+}:
+appimageTools.wrapType2 rec {
+  pname = "beeper";
+  version = "3.79.17";
+
+  src = fetchurl {
+    url = "https://download.beeper.com/linux/appImage/x64";
+    sha256 = "lBw+IlBdSI7gELbCL0nY74inZF8iL3fBtOJzZaD8t9Y=";
+  };
+
+  extraPkgs = pkgs:
+    with pkgs; [
+      libsecret
+    ];
+
+  extraInstallCommands =
+    let
+      contents = appimageTools.extractType2 { inherit pname version src; };
+    in
+    ''
+      source ${makeWrapper}/nix-support/setup-hook
+      mv $out/bin/${pname}-${version} $out/bin/${pname}
+      wrapProgram $out/bin/${pname} \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland}}"
+
+      install -m 444 -D ${contents}/beeper.desktop -t $out/share/applications
+      substituteInPlace $out/share/applications/beeper.desktop \
+        --replace 'Exec=AppRun' 'Exec=${pname}'
+      cp -r ${contents}/usr/share/icons $out/share
+    '';
+
+  passthru = {
+    updateScript = writeShellScript "update-beeper" ''
+      set -o errexit
+      set -x
+      export PATH="${lib.makeBinPath [curl gnugrep pcre common-updater-scripts]}"
+      version="$(curl -sI -X GET https://download.beeper.com/linux/appImage/x64 | grep -Fi 'content-disposition:' | pcregrep -o1 'beeper-(([0-9]\.?)+[0-9])')"
+      update-source-version beeper "$version"
+    '';
+  };
+
+  meta = with lib; {
+    description = "All your chats in one app.";
+    homepage = "https://beeper.com";
+    license = licenses.unfree;
+    maintainers = [ ];
+    platforms = [ "x86_64-linux" ];
+  };
+}
--- WickedWizard/Programs/Proprietary/beeper.nix
+++ WickedWizard/Programs/Proprietary/beeper.nix
@@ -1,5 +1,9 @@
-{ config, pkgs, ... }: {
-  home.packages = with pkgs; [ beeper ];
+{ config, pkgs, ... }:
+let
+  beeper-desktop = pkgs.callPackage ../../../Packages/beeper.nix { };
+in
+{
+  home.packages = with pkgs; [ beeper-desktop ];
 
   xdg.configFile."autostart/beeper.desktop".source = "${pkgs.beeper}/share/applications/beeper.desktop";
 }
