d372e3d24646e08a2dcf9f4a1e0745694469fec6
WickedWizard3588 <coding.exhibiting@slmails.com>
2023-10-18 13:23:45 +0530

    feat(CS): automate CS class

--- Users/WickedWizard/Programs/Browsers/chromium.nix
+++ Users/WickedWizard/Programs/Browsers/chromium.nix
@@ -4,4 +4,15 @@
   home.sessionVariables = {
     CHROME_USER_DATA_DIR = "$HOME/Documents/Browsers/Chromium";
   };
+
+  xdg.desktopEntries = {
+    Computer_Class = {
+      name = "Computer Class";
+      genericName = "Class";
+      exec = "sh -c \"chromium --new-window "https://app.zoom.us/wc/3095436847/join?from=join&pwd=srgs@omr" --new-window "https://app.zoom.us/wc/79849915949/join?from=join&pwd=barbie"; obs\"";
+      terminal = false;
+      icon = "chromium";
+      type = "Application";
+    };
+  };
 }
