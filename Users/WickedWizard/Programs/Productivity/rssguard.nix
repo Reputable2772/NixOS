a03e34b3bcc31d18594c2f45fc869603e8b06176
WickedWizard3588 <coding.exhibiting@slmails.com>
2023-10-20 18:16:13 +0530

    fix(rssguard): remove rssguard from autostart

--- Users/WickedWizard/Programs/Productivity/rssguard.nix
+++ Users/WickedWizard/Programs/Productivity/rssguard.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [ rssguard ];
 
-  xdg.configFile."autostart/rssguard.desktop".source = "${pkgs.rssguard}/share/applications/io.github.martinrotter.rssguard.desktop";
+  # xdg.configFile."autostart/rssguard.desktop".source = "${pkgs.rssguard}/share/applications/io.github.martinrotter.rssguard.desktop";
 }
