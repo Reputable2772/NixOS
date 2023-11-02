0179fd7d03a7e5ff651ac9c0adb3d3b9f6817587
WickedWizard3588 <coding.exhibiting@slmails.com>
2023-11-02 09:10:47 +0530

    feat(cachix): build on github actions instead of locally

--- Overlays/prismlauncher-qt5.nix
+++ Overlays/prismlauncher-qt5.nix
@@ -1,7 +1,7 @@
 final: prev: {
   prismlauncher-qt5-unwrapped = prev.prismlauncher-qt5-unwrapped.overrideAttrs (old: {
     patches = [ ] ++ [
-      ../Users/WickedWizard/Programs/Games/crack.patch
+      ./prismlauncher-qt5-crack.patch
     ];
   });
 }
