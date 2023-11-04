51af4df6113b2eccb5ec1f51be61cb13779af77d
WickedWizard3588 <coding.exhibiting@slmails.com>
2023-11-04 10:20:28 +0530

    feat(nix.sh): make dconf building optional

--- nix.sh
+++ nix.sh
@@ -6,20 +6,24 @@ echo "Formatting and checking files."
 nix fmt
 nix flake check
 
-echo "Dumping DConf"
-dconf dump / > wickedwizard.dconf
-sudo -u shuba dconf dump / > shuba.dconf
+if [ -n "$1" ] then
+	echo "Dumping DConf"
+	dconf dump / > wickedwizard.dconf
+	sudo -u shuba dconf dump / > shuba.dconf
 
-sed -e 's/\\n//g' wickedwizard.dconf > wickedwizard.conf
-sed -e 's/\\n//g' shuba.dconf > shuba.conf
+	sed -e 's/\\n//g' wickedwizard.dconf > wickedwizard.conf
+	sed -e 's/\\n//g' shuba.dconf > shuba.conf
 
-echo "DConf2Nix"
+	rm -rf ./wickedwizard.dconf
+	rm -rf ./shuba.dconf
 
-dconf2nix -i wickedwizard.conf -o ./Users/WickedWizard/Programs/Desktop/Gnome/gnome.nix
-dconf2nix -i shuba.conf -o ./Users/Shuba/gnome.nix
+	echo "DConf2Nix"
+	dconf2nix -i wickedwizard.conf -o ./Users/WickedWizard/Programs/Desktop/Gnome/gnome.nix
+	dconf2nix -i shuba.conf -o ./Users/Shuba/gnome.nix
 
-rm -rf ./wickedwizard.dconf ./wickedwizard.conf
-rm -rf ./shuba.dconf ./shuba.conf
+	rm -rf ./wickedwizard.conf
+	rm -rf ./shuba.conf
+fi
 
 echo "Actually builiding"
 rm -rf $HOME/.config/mimeapps.list
