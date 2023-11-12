198aca08c8b76b8064fe51f6d1dcead74422faf4
WickedWizard3588 <coding.exhibiting@slmails.com>
2023-11-12 15:05:37 +0530

    chore(sensitive): remove sensitive data completely

--- Users/WickedWizard/Services/backup.nix
+++ Users/WickedWizard/Services/backup.nix
@@ -3,16 +3,13 @@
     backup = {
       Service = {
         Type = "oneshot";
-        ExecStart = "${pkgs.lib.getExe' pkgs.restic "restic"} -r /home/wickedwizard/Backups --password-file /home/wickedwizard/Documents/NixOS/Sensitive/Backup-Key.txt backup /home/wickedwizard/Documents/; ${pkgs.lib.getExe' pkgs.rclone "rclone"} --verbose sync /home/wickedwizard/Backups backup:Backups";
+        ExecStart = "${pkgs.lib.getExe' pkgs.restic "restic"} -r /home/wickedwizard/Backups --password-file /home/wickedwizard/Documents/Config/Backup-Key.txt backup /home/wickedwizard/Documents/; ${pkgs.lib.getExe' pkgs.rclone "rclone"} --verbose sync /home/wickedwizard/Backups backup:Backups";
       };
       Unit = {
         Description = "Restic Backup";
         After = [ "network-online.target" ];
         Wants = [ "network-online.target" ];
       };
-      # Timer = {
-      #   OnUnitActiveSec = "18h";
-      # };
     };
   };
 }
