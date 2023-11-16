f9442693a233b049bdec63aa616d17bd73327169
WickedWizard3588 <coding.exhibiting@slmails.com>
2023-11-16 23:09:08 +0530

    fix(paths): stop hardcoding paths

--- Users/WickedWizard/Services/backup.nix
+++ Users/WickedWizard/Services/backup.nix
@@ -3,7 +3,7 @@
     backup = {
       Service = {
         Type = "oneshot";
-        ExecStart = "${pkgs.lib.getExe' pkgs.restic "restic"} -r /home/wickedwizard/Backups --password-file /home/wickedwizard/Documents/Config/Backup-Key.txt backup /home/wickedwizard/Documents/; ${pkgs.lib.getExe' pkgs.rclone "rclone"} --verbose sync /home/wickedwizard/Backups backup:Backups";
+        ExecStart = "${pkgs.lib.getExe' pkgs.restic "restic"} -r ${config.home.homeDirectory}/Backups --password-file ${config.home.homeDirectory}/Documents/Config/Backup-Key.txt backup ${config.home.homeDirectory}/Documents/; ${pkgs.lib.getExe' pkgs.rclone "rclone"} --verbose sync ${config.home.homeDirectory}/Backups backup:Backups";
       };
       Unit = {
         Description = "Restic Backup";
