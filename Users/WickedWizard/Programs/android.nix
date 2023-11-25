fee5ba1f2c3cc29b854b4bd86f5f2138c0c997d1
WickedWizard3588 <coding.exhibiting@slmails.com>
2023-11-25 10:16:43 +0530

    feat(waydroid): add waydroid_scripts

--- Users/WickedWizard/Programs/android.nix
+++ Users/WickedWizard/Programs/android.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [
+    # ADB and related stuff
     android-tools
     android-udev-rules
+
+    # Waydroid related stuff
+    nur.repos.ataraxiasjel.waydroid-script
   ];
 }
