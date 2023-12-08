a1a7addc5ced57cf3a5066c41f424782ec8cc141
WickedWizard3588 <coding.exhibiting@slmails.com>
2023-12-08 23:12:59 +0530

    fix(discord): remove discord autostart

--- Users/WickedWizard/Programs/Proprietary/discord.nix
+++ Users/WickedWizard/Programs/Proprietary/discord.nix
@@ -5,5 +5,5 @@
     })
   ];
 
-  xdg.configFile."autostart/discord.desktop".source = "${pkgs.discord}/share/applications/discord.desktop";
+  # xdg.configFile."autostart/discord.desktop".source = "${pkgs.discord}/share/applications/discord.desktop";
 }
