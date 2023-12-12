7b02e714cf8ae1889ad1e3745f74601eaa514fff
Reputable2772 <coding.exhibiting@slmails.com~>
2023-12-12 11:50:52 +0530

    fix(git): fix git ssh signing keys

--- Users/WickedWizard/Programs/Shell/git.nix
+++ Users/WickedWizard/Programs/Shell/git.nix
@@ -11,10 +11,11 @@
     extraConfig = {
       gpg = {
         format = "ssh";
+        ssh.allowedSignersFile = "HOME/.config/git/allowed_signers";
       };
     };
     signing = {
-      key = "/home/wickedwizard/Documents/Dotfiles/Scripts/SSH/id_rsa.pub";
+      key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC05WkGXPooEYhfb2R6cTq/GBEq739jmyfcuMqXUG6Xpz5/TIvCvpjDU2vm7XyOCnThklXrPwcixavl4DunTxIs8kSTLp7u2TkMzxM3e7JxNlBUJGy4OMEMFeocsrbMjh9utd+K+PBgGvM/rsEkLMYGbOGRsS+476stZwCa6Sa8aE5TENuW6Bp0SNf4HwdHdIo+/U9yUtEgnM8SP7kCGCUYIcFoXB5jPdHujG23snvF68bwg5i7WBp/PmhpHhV1aMDkSrzRw2wx/zOISIHU23bX8L5rOiTTqWU6yZQ5T9YutKcOOF6eA1vtccJ6JuiahusbMYYTYNyh0IqY6wszO7P4Fl/KZ3/0E6x5atzv+m8DslEsOBiEBu2Nuv0359OiEOH5u6psbjw1KYEtpAf3UwqIT5Cc9mDiOyHTrQQgEddv+IM1vz44Mhc5In66I509APIzjGNGzJ9s9RoRzmhOvjezWak69+rUIr/SuUHzhrvsa9cpG5ASP431tvKmEuD4BXREw32Z5oMt789C/jtknoROu3sWVSeMxKPj8m5Wvq1KyoCJ0biJhHizGV2tvssPJbhBduOWlstK4FlUScFTWlW4doBA3yKOOa25KmRsfiQkQ8pjid5shIJ6CDzsSjIdkT8tSegr2/RzlL9EVC1xii/zAXOqV/+x4SOYD8MdvqXVKw== wickedwizard@hp-laptop";
       signByDefault = true;
     };
   };
