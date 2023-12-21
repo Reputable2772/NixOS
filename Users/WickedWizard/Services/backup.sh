2d20326422bb947d56da64d1dfce4433a8a68bdf
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2023-12-21 17:20:18 +0530

    feat(backup): directly use rclone backups

--- Users/WickedWizard/Services/backup.sh
+++ Users/WickedWizard/Services/backup.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-echo "Creating Backup"
-/etc/profiles/per-user/wickedwizard/bin/restic -r /home/wickedwizard/Backups --password-file /home/wickedwizard/Documents/Config/Backup-Key.txt backup /home/wickedwizard/Documents/
-
-echo "Syncing Backups"
-/etc/profiles/per-user/wickedwizard/bin/rclone --verbose sync /home/wickedwizard/Backups backup:Backups
+echo "Creating and Syncing Backup"
+/etc/profiles/per-user/wickedwizard/bin/restic --verbose --password-file /home/wickedwizard/Documents/Config/Backup-Key.txt --repo rclone:backup:Backups backup /home/wickedwizard/Documents/
