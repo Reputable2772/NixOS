974e102869b4b8c0f3f87f0e2b1f2728a881dfc3
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2023-12-22 20:33:23 +0530

    fix(librewolf): manage librewolf declaratively

--- Users/WickedWizard/Programs/Browsers/librewolf.nix
+++ Users/WickedWizard/Programs/Browsers/librewolf.nix
@@ -1,5 +1,13 @@
 { config, pkgs, ... }: {
-  home.packages = with pkgs; [ librewolf ];
+  programs.librewolf = {
+    enable = true;
+    settings = {
+      "privacy.resistFingerprinting.letterboxing" = true;
+      "network.http.referer.XOriginPolicy" = 2;
+      "privacy.clearOnShutdown.history" = false;
+      "privacy.clearOnShutdown.downloads" = false;
+    };
+  };
 
   # xdg.configFile."autostart/librewolf.desktop".source = "${pkgs.librewolf}/share/applications/librewolf.desktop";
 }
