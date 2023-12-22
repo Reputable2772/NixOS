20295aa2da4a56f52456a30fead2f30cb996c0b4
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2023-12-22 20:25:35 +0530

    feat(firefox-gnome-theme): utilize firefox-gnome-theme

--- Users/WickedWizard/Programs/Browsers/firefox.nix
+++ Users/WickedWizard/Programs/Browsers/firefox.nix
@@ -1,10 +1,21 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  firefox-gnome-theme = pkgs.callPackage ../../../../Packages/firefox-gnome-theme.nix { };
+in {
+  home.packages = with pkgs; [ firefox-gnome-theme ];
   programs.firefox = {
     enable = true;
     profiles."default" = {
       isDefault = true;
       name = "default";
       path = "../../Documents/Browsers/Firefox/";
+      userChrome = ''
+        @import "${firefox-gnome-theme}/share/firefox-gnome-theme/userChrome.css";
+      '';
+      userContent = ''
+        @import "${firefox-gnome-theme}/share/firefox-gnome-theme/userContent.css";
+      '';
+      extraConfig = builtins.readFile "${firefox-gnome-theme}/share/firefox-gnome-theme/configuration/user.js";
     };
   };
 
