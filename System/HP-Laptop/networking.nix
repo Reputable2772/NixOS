77250a5104af7520ee2f7785e569bc8e3c95666b
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-01-02 15:52:16 +0530

    fix(dns): use DoT to prevent DNS hijacking

--- System/HP-Laptop/networking.nix
+++ System/HP-Laptop/networking.nix
@@ -23,4 +23,22 @@
 
   networking.hostName = "hp-laptop";
   networking.networkmanager.enable = true;
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    extraConfig = ''
+      DNSOverTLS=opportunistic
+      MulticastDNS=resolve
+    '';
+    llmnr = "true";
+  };
+
+  networking.nameservers = [
+    "1.1.1.1#cloudflare-dns.com"
+    "1.0.0.1#cloudflare-dns.com"
+    "2606:4700:4700::1111#cloudflare-dns.com"
+    "2606:4700:4700::1001#cloudflare-dns.com"
+  ];
 }
