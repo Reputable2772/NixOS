e5b5d325482284063dcc6a27295131efae4f2823
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-01-14 11:04:01 +0530

    feat(firewall): simplify firewall rules

--- System/HP-Laptop/networking.nix
+++ System/HP-Laptop/networking.nix
@@ -1,24 +1,21 @@
 {
   networking.firewall = {
     enable = true;
-    interfaces.eno1.allowedTCPPortRanges = [
+    allowedTCPPortRanges = [
       { from = 1714; to = 1764; } # KDE Connect
     ];
-    interfaces.eno1.allowedUDPPortRanges = [
+    allowedUDPPortRanges = [
       { from = 1714; to = 1764; } # KDE Connect
     ];
-    interfaces.eno1.allowedTCPPorts = [ 8384 22000 443 80 ]; # Syncthing, Caddy
-    interfaces.eno1.allowedUDPPorts = [ 22000 21027 443 80 ]; # Syncthing
+    allowedTCPPorts =
+      [ 22000 ] # Syncthing
+      ++ [ 443 80 ] # Caddy
+      ++ [ 61851 ]; # qBittorrent
+    allowedUDPPorts =
+    [ 22000 21027 ] # Syncthing
+    ++ [ 443 80 ] # Caddy
+    ++ [ 61851 ]; # qBittorrent
     logRefusedPackets = true;
-
-    interfaces.wlo1.allowedTCPPortRanges = [
-      { from = 1714; to = 1764; } # KDE Connect
-    ];
-    interfaces.wlo1.allowedUDPPortRanges = [
-      { from = 1714; to = 1764; } # KDE Connect
-    ];
-    interfaces.wlo1.allowedTCPPorts = [ 8384 22000 443 80 ]; # Syncthing, Caddy
-    interfaces.wlo1.allowedUDPPorts = [ 22000 21027 443 80 ]; # Syncthing
   };
 
   networking.hostName = "hp-laptop";
