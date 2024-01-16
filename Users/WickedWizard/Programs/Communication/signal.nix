a10b2478d308549af22a1b191279f22552b986a9
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-01-16 20:08:49 +0530

    feat(signal): add signal to autostart

--- Users/WickedWizard/Programs/Communication/signal.nix
+++ Users/WickedWizard/Programs/Communication/signal.nix
@@ -1,3 +1,5 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [ signal-desktop ];
+
+  xdg.configFile."autostart/signal.desktop".source = "${pkgs.signal-desktop}/share/applications/signal-desktop.desktop";
 }
