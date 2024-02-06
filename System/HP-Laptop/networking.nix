e2a4b7dce31ea68e363453184fa097c3b06f9167
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-02-06 16:53:54 +0530

    feat(ipv6): enable privacy extensions

--- System/HP-Laptop/networking.nix
+++ System/HP-Laptop/networking.nix
@@ -1,26 +1,41 @@
 {
-  networking.firewall = {
-    enable = true;
-    allowedTCPPortRanges = [
-      { from = 1714; to = 1764; } # KDE Connect
+  networking = {
+    hostName = "hp-laptop";
+    firewall = {
+      enable = true;
+      allowedTCPPortRanges = [
+        { from = 1714; to = 1764; } # KDE Connect
+      ];
+      allowedUDPPortRanges = [
+        { from = 1714; to = 1764; } # KDE Connect
+      ];
+      allowedTCPPorts =
+        [ 53 ] # DNS
+        ++ [ 80 443 ] # Caddy
+        ++ [ 22000 ] # Syncthing
+        ++ [ 61851 ]; # qBittorrent
+      allowedUDPPorts =
+        [ 80 443 ] # Caddy
+        ++ [ 22000 21027 ] # Syncthing
+        ++ [ 61851 ]; # qBittorrent
+      logRefusedPackets = true;
+    };
+    nameservers = [
+      "1.1.1.1#cloudflare-dns.com"
+      "1.0.0.1#cloudflare-dns.com"
+      "2606:4700:4700::1111#cloudflare-dns.com"
+      "2606:4700:4700::1001#cloudflare-dns.com"
     ];
-    allowedUDPPortRanges = [
-      { from = 1714; to = 1764; } # KDE Connect
-    ];
-    allowedTCPPorts =
-      [ 53 ] # DNS
-      ++ [ 80 443 ] # Caddy
-      ++ [ 22000 ] # Syncthing
-      ++ [ 61851 ]; # qBittorrent
-    allowedUDPPorts =
-      [ 80 443 ] # Caddy
-      ++ [ 22000 21027 ] # Syncthing
-      ++ [ 61851 ]; # qBittorrent
-    logRefusedPackets = true;
+    networkmanager = {
+      enable = true;
+      extraConfig = ''
+        [connection]
+        ipv6.ip6-privacy=2
+        [ipv6]
+        addr-gen-mode=stable-privacy
+      '';
+    };
   };
-
-  networking.hostName = "hp-laptop";
-  networking.networkmanager.enable = true;
 
   services.resolved = {
     enable = true;
@@ -33,11 +48,4 @@
     '';
     llmnr = "true";
   };
-
-  networking.nameservers = [
-    "1.1.1.1#cloudflare-dns.com"
-    "1.0.0.1#cloudflare-dns.com"
-    "2606:4700:4700::1111#cloudflare-dns.com"
-    "2606:4700:4700::1001#cloudflare-dns.com"
-  ];
 }
