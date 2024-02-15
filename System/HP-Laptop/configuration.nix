880263ebf83f6d976a3b5bc9b6ceed789d390603
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-02-15 13:14:26 +0530

    feat(configuration.nix): make configuration.nix extensible

--- System/HP-Laptop/configuration.nix
+++ System/HP-Laptop/configuration.nix
@@ -1,7 +1,8 @@
 { config, pkgs, inputs, ... }:
+let
+  inherit (inputs) nur home-manager flatpaks nixpkgs;
+in
 {
-  nix.settings.trusted-users = [ "wickedwizard" ];
-
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
     "beeper"
     "discord"
@@ -10,12 +11,14 @@
     "steam-original"
     "steam-run"
   ];
+  nixpkgs.overlays = [ nur.overlay ] ++ import ../../Overlays;
 
   # Needs to be set here or else shell won't work
   programs.zsh.enable = true;
   # Shell autocomplete
   environment.pathsToLink = [ "/share/zsh" ];
 
+  nix.settings.trusted-users = [ "wickedwizard" ];
   users.users.wickedwizard = {
     isNormalUser = true;
     home = "/home/wickedwizard";
@@ -24,7 +27,25 @@
     shell = pkgs.zsh;
   };
 
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users = {
+      wickedwizard = {
+        imports = [
+          ../../Users/WickedWizard/home.nix
+          flatpaks.homeManagerModules.default
+        ];
+      };
+    };
+    extraSpecialArgs = {
+      inherit inputs;
+    };
+  };
+
   imports = [
+    nur.nixosModules.nur
+    home-manager.nixosModules.home-manager
     ../configuration.nix
   ];
 }
--- System/configuration.nix
+++ System/configuration.nix
@@ -1,32 +1,6 @@
 { config, pkgs, inputs, ... }:
-let
-  inherit (inputs) nur home-manager flatpaks nixpkgs;
-in
 {
   time.timeZone = "Asia/Kolkata";
-
-  imports = [
-    nur.nixosModules.nur
-    home-manager.nixosModules.home-manager
-    {
-      nixpkgs.overlays = [ nur.overlay ] ++ (import ../Overlays);
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        users = {
-          wickedwizard = {
-            imports = [
-              ../Users/WickedWizard/home.nix
-              flatpaks.homeManagerModules.default
-            ];
-          };
-        };
-        extraSpecialArgs = {
-          inherit inputs;
-        };
-      };
-    }
-  ];
 
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
