a50d29aaaa3ad6433ed788cef341bf8a83d40528
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-02-26 17:58:27 +0530

    ci(overlays): fix ci overlay
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- nix.sh
+++ nix.sh
@@ -27,7 +27,7 @@ ci() {
 	mv ./Overlays/default.nix ./Overlays/default.nix.test
 
 	for overlay in ./Overlays/*.nix; do
-		nix-build -I "$NIX_PATH:nixpkgs-overlays=$PWD/Overlays" -E "with import <nixpkgs> {}; callPackage $overlay {}"
+		nix-build -I "$NIX_PATH:nixpkgs-overlays=$PWD/Overlays" -E "with import <nixpkgs> {}; callPackage $(echo $overlay | cut -d '/' -f3 <<< "$s" | cut -d '.' -f1 <<< "$s") {}"
 	done
 
 	mv ./Overlays/default.nix.test ./Overlays/default.nix
