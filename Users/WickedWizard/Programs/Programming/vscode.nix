6880fe30cc757ed542fc1e10bad4262fc84983e6
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-03-03 18:26:41 +0530

    feat(vscode): don't trim whitespace in diff
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Programming/vscode.nix
+++ Users/WickedWizard/Programs/Programming/vscode.nix
@@ -7,9 +7,7 @@
       eamodio.gitlens
       github.vscode-github-actions
       pkief.material-icon-theme
-      jnoortheen.nix-ide
       editorconfig.editorconfig
-      rust-lang.rust-analyzer
       arcticicestudio.nord-visual-studio-code
       mkhl.direnv
       oderwat.indent-rainbow
@@ -19,6 +17,32 @@
       "explorer.confirmDragAndDrop" = false;
       "workbench.colorTheme" = "Nord";
       "workbench.iconTheme" = "material-icon-theme";
+      "diffEditor.ignoreTrimWhitespace" = false;
     };
   };
+
+  xdg.dataFile.".vscode-oss/argv.json".text = ''
+    // This configuration file allows you to pass permanent command line arguments to VS Code.
+    // Only a subset of arguments is currently supported to reduce the likelihood of breaking
+    // the installation.
+    //
+    // PLEASE DO NOT CHANGE WITHOUT UNDERSTANDING THE IMPACT
+    //
+    // NOTE: Changing this file requires a restart of VS Code.
+    {
+      // Use software rendering instead of hardware accelerated rendering.
+      // This can help in cases where you see rendering issues in VS Code.
+      // "disable-hardware-acceleration": true,
+
+      // Allows to disable crash reporting.
+      // Should restart the app if the value is changed.
+      "enable-crash-reporter": false,
+
+      // Unique id used for correlating crash reports sent from this instance.
+      // Do not edit this value.
+      "crash-reporter-id": "259850d6-487b-4bc4-9ea6-875cc91ac2bf",
+
+      "password-store": "gnome-libsecret"
+    }
+  '';
 }
