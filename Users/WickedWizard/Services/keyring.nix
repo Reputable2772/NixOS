d290d17be30acf46d757f04310fe31a06edb9f44
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-03-03 10:51:58 +0530

    revert(keyring): remove global gnome keyring
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Services/keyring.nix
+++ Users/WickedWizard/Services/keyring.nix
@@ -1,3 +1,8 @@
 {
   services.gnome-keyring.enable = true;
+
+  # Issues https://github.com/nix-community/home-manager/issues/1600 https://github.com/NixOS/nixpkgs/issues/193835
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
+  };
 }
