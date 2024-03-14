371935bab912467772ddb244798f3e2560fa9505
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-03-14 23:19:59 +0530

    fix(xdg): hotfix broken import
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Misc/default.nix
+++ System/HP-Laptop/Misc/default.nix
@@ -1,6 +1,6 @@
-{ config, ... }: {
+{
   imports = [
     ./sudo.nix
-  ]
-  ++ lib.optional (config.programs.hyprland.enable) ./xdg-systemd.nix;
+    ./xdg-systemd.nix
+  ];
 }
--- System/HP-Laptop/Misc/xdg-systemd.nix
+++ System/HP-Laptop/Misc/xdg-systemd.nix
@@ -1,6 +1,6 @@
 # Fixes https://github.com/NixOS/nixpkgs/issues/189851#issuecomment-1238907955
-{
-  systemd.user.extraConfig = ''
+{ config, lib, ... }: {
+  systemd.user.extraConfig = lib.mkIf (config.programs.hyprland.enable) ''
     DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
   '';
 }
