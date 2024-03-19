5a73d0c3931481d22200c3383da909ddc555306b
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-03-19 10:37:21 +0530

    feat(flameshot): make overlay optional, but build it regularly
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Overlays/default.nix
+++ Overlays/default.nix
@@ -1,12 +1,10 @@
 let
   dconf2nix = import ./dconf2nix.nix;
-  flameshot = import ./flameshot.nix;
   lutris-unwrapped = import ./lutris.nix;
   prismlauncher-unwrapped = import ./prismlauncher-qt5-unwrapped.nix;
 in
 [
   dconf2nix
-  flameshot
   lutris-unwrapped
   prismlauncher-unwrapped
 ]
