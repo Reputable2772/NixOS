6e040322403ad6e03306039cc4be60939c6ab191
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-03-21 12:22:04 +0530

    feat(ssh): set env variable
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Services/keyring.nix
+++ System/HP-Laptop/Services/keyring.nix
@@ -1,3 +1,8 @@
 {
   services.gnome.gnome-keyring.enable = true;
+
+  # Fixes https://github.com/NixOS/nixpkgs/issues/61539#issuecomment-524653963
+  environment.sessionVariables = {
+    SSH_AUTH_SOCK = "/run/user/$UID/keyring/ssh";
+  };
 }
