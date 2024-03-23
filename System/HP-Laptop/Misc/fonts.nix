bb4deed0eaec036d12cb842a3a9c2348e5e3c89a
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-03-23 10:43:27 +0530

    feat(fonts): enable custom fonts
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

---
+++ System/HP-Laptop/Misc/fonts.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }: {
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      font-awesome
+    ];
+  };
+}
