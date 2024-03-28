51f4a33555cb6d4647a77e985aadc9e1eed77a3d
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-03-28 16:50:27 +0530

    chore(dconf): cleanup dconf variables
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Desktop/Gnome/dconf.nix
+++ Users/WickedWizard/Programs/Desktop/Gnome/dconf.nix
@@ -5,27 +5,9 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
-    "apps/seahorse/listing" = {
-      item-filter = "any";
-      keyrings-selected = [ "gnupg://" ];
-    };
-
-    "apps/seahorse/windows/key-manager" = {
-      height = 991;
-      width = 1878;
-    };
-
-    "com/belmoussaoui/Authenticator" = {
-      keyrings-migrated = true;
-    };
-
     "com/github/wwmm/easyeffects" = {
       last-used-input-preset = "Presets";
       last-used-output-preset = "Presets";
-      window-fullscreen = false;
-      window-height = 429;
-      window-maximized = true;
-      window-width = 749;
     };
 
     "com/github/wwmm/easyeffects/streaminputs" = {
@@ -33,59 +15,25 @@ with lib.hm.gvariant;
     };
 
     "com/github/wwmm/easyeffects/streamoutputs" = {
-      output-device = "bluez_output.41_42_4C_5C_BB_63.1";
-    };
-
-    "de/schmidhuberj/tubefeeder" = {
-      is-maximized = true;
-      player = "mpv";
-      window-height = 650;
-      window-width = 850;
-    };
-
-    "io/github/lainsce/Khronos" = {
-      schema-version = 1;
+      output-device = "alsa_output.pci-0000_04_00.6.analog-stereo";
     };
 
     "org/gnome/Fractal/Stable" = {
-      current-session = "7ecb27dd-f8c4-400c-8956-2d488cac633d";
+      current-session = "85cd8322-bace-427a-a816-98d74e3aa12e";
       is-maximized = true;
       markdown-enabled = true;
-      sessions = "[[\"7ecb27dd-f8c4-400c-8956-2d488cac633d\",{}]]";
+      sessions = "[[\"85cd8322-bace-427a-a816-98d74e3aa12e\",{}]]";
       window-height = 602;
       window-width = 763;
     };
 
-    "org/gnome/control-center" = {
-      last-panel = "power";
-      window-state = mkTuple [ 980 640 true ];
-    };
-
     "org/gnome/desktop/interface" = {
-      clock-show-weekday = false;
       color-scheme = "prefer-dark";
       cursor-theme = "Nordic-cursors";
       font-antialiasing = "grayscale";
       font-hinting = "slight";
-      gtk-theme = "adw-gtk3-dark";
+      gtk-theme = "Catppuccin-Mocha-Standard-Blue-Dark";
       icon-theme = "Adwaita";
-    };
-
-    "org/gnome/desktop/notifications/application/com-belmoussaoui-authenticator" = {
-      application-id = "com.belmoussaoui.Authenticator.desktop";
-    };
-
-    "org/gnome/desktop/notifications/application/vesktop" = {
-      application-id = "vesktop.desktop";
-    };
-
-    "org/gnome/desktop/notifications/application/vlc" = {
-      application-id = "vlc.desktop";
-    };
-
-    "org/gnome/desktop/peripherals/touchpad" = {
-      tap-to-click = false;
-      two-finger-scrolling-enabled = true;
     };
 
     "org/gnome/evince/default" = {
@@ -97,15 +45,9 @@ with lib.hm.gvariant;
       inverted-colors = true;
       show-sidebar = true;
       sidebar-page = "thumbnails";
-      sidebar-size = 132;
+      sidebar-size = 194;
       sizing-mode = "fit-width";
-      window-ratio = mkTuple [ 1.615126 0.25772 ];
-      zoom = 1.118202;
-    };
-
-    "org/gnome/file-roller/dialogs/extract" = {
-      recreate-folders = true;
-      skip-newer = false;
+      window-ratio = mkTuple [ 0.9803921568627451 0.7575757575757576 ];
     };
 
     "org/gnome/file-roller/listing" = {
@@ -118,51 +60,8 @@ with lib.hm.gvariant;
 
     "org/gnome/file-roller/ui" = {
       sidebar-width = 200;
-      window-height = 991;
+      window-height = 948;
       window-width = 1878;
-    };
-
-    "org/gnome/nautilus/preferences" = {
-      default-folder-viewer = "icon-view";
-      migrated-gtk-settings = true;
-      search-filter-time-type = "last_modified";
-    };
-
-    "org/gnome/nautilus/window-state" = {
-      initial-size = mkTuple [ 890 550 ];
-      maximized = true;
-    };
-
-    "org/gnome/portal/filechooser/kitty" = {
-      last-folder-path = "/home/wickedwizard/Downloads";
-    };
-
-    "org/gnome/portal/filechooser/org/gnome/Settings" = {
-      last-folder-path = "/home/wickedwizard/Documents/Applications/Containers/Lidarr";
-    };
-
-    "org/gnome/shell/extensions/gsconnect" = {
-      devices = [ "9e744f80_c5dc_4789_896d_0da157ad91ea" ];
-      id = "78f577aa-968b-4376-b114-65cb48753f9b";
-      name = "hp-laptop";
-    };
-
-    "org/gnome/shell/extensions/gsconnect/device/9e744f80_c5dc_4789_896d_0da157ad91ea" = {
-      incoming-capabilities = [ "kdeconnect.battery" "kdeconnect.bigscreen.stt" "kdeconnect.clipboard" "kdeconnect.clipboard.connect" "kdeconnect.contacts.request_all_uids_timestamps" "kdeconnect.contacts.request_vcards_by_uid" "kdeconnect.findmyphone.request" "kdeconnect.mousepad.keyboardstate" "kdeconnect.mousepad.request" "kdeconnect.mpris" "kdeconnect.mpris.request" "kdeconnect.notification" "kdeconnect.notification.action" "kdeconnect.notification.reply" "kdeconnect.notification.request" "kdeconnect.ping" "kdeconnect.runcommand" "kdeconnect.sftp.request" "kdeconnect.share.request" "kdeconnect.share.request.update" "kdeconnect.sms.request" "kdeconnect.sms.request_attachment" "kdeconnect.sms.request_conversation" "kdeconnect.sms.request_conversations" "kdeconnect.systemvolume" "kdeconnect.telephony.request" "kdeconnect.telephony.request_mute" ];
-      last-connection = "lan://192.168.0.103:1716";
-      name = "Redmi Y2";
-      outgoing-capabilities = [ "kdeconnect.battery" "kdeconnect.bigscreen.stt" "kdeconnect.clipboard" "kdeconnect.clipboard.connect" "kdeconnect.connectivity_report" "kdeconnect.contacts.response_uids_timestamps" "kdeconnect.contacts.response_vcards" "kdeconnect.findmyphone.request" "kdeconnect.mousepad.echo" "kdeconnect.mousepad.keyboardstate" "kdeconnect.mousepad.request" "kdeconnect.mpris" "kdeconnect.mpris.request" "kdeconnect.notification" "kdeconnect.notification.request" "kdeconnect.ping" "kdeconnect.presenter" "kdeconnect.runcommand.request" "kdeconnect.sftp" "kdeconnect.share.request" "kdeconnect.sms.attachment_file" "kdeconnect.sms.messages" "kdeconnect.systemvolume.request" "kdeconnect.telephony" ];
-      paired = false;
-      supported-plugins = [ "battery" "clipboard" "connectivity_report" "contacts" "findmyphone" "mousepad" "mpris" "notification" "ping" "presenter" "runcommand" "sftp" "share" "sms" "systemvolume" "telephony" ];
-      type = "phone";
-    };
-
-    "org/gnome/tweaks" = {
-      show-extensions-notice = false;
-    };
-
-    "org/gtk/gtk4/settings/emoji-chooser" = {
-      recent-emoji = [ (mkTuple [ (mkTuple [ [ (mkUint32 128517) ] "grinning face with sweat" [ "cold" "face" "open" "smile" "sweat" ] (mkUint32 0) ]) (mkUint32 0) ]) ];
     };
 
     "org/gtk/settings/file-chooser" = {
@@ -171,14 +70,13 @@ with lib.hm.gvariant;
       show-hidden = false;
       show-size-column = true;
       show-type-column = true;
-      sidebar-width = 203;
-      sort-column = "modified";
+      sidebar-width = 179;
+      sort-column = "name";
       sort-directories-first = false;
-      sort-order = "descending";
+      sort-order = "ascending";
       type-format = "category";
       window-position = mkTuple [ 0 0 ];
-      window-size = mkTuple [ 933 466 ];
+      window-size = mkTuple [ 1231 902 ];
     };
-
   };
 }
