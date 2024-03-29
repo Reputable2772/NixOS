6a555ae54ec910567fa065883957e53d78a4c493
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-03-29 18:01:00 +0530

    feat(theming): fix consistency
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Desktop/Theming/icons.nix
+++ Users/WickedWizard/Programs/Desktop/Theming/icons.nix
@@ -4,6 +4,6 @@
     package = pkgs.gnome.adwaita-icon-theme;
   };
   dconf.settings."org/gnome/desktop/interface" = {
-    icon-theme = lib.mkForce config.gtk.iconTheme;
+    icon-theme = lib.mkForce config.gtk.iconTheme.name;
   };
 }
