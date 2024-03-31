dffb087c7797db3f08f6a12e2215a07efbe93ee6
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-03-31 20:16:53 +0530

    fix(filen): rewrite filen package
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Packages/filen.nix
+++ Packages/filen.nix
@@ -1,41 +1,47 @@
 { lib
-, appimageTools
-, makeDesktopItem
 , fetchurl
+, makeWrapper
+, appimageTools
+, libappindicator-gtk3
 }:
 let
-  icon = fetchurl {
-    url = "https://filen.io/images/logo_dark.svg";
-    sha256 = "sha256-t8Ux+hfH21QPsuDWy9Q9a1aE1INUnQh70ZpwLUx1i+4=";
-  };
-  description = "Filen provides cheap end-to-end encrypted cloud storage (zero-knowledge principle).";
-  desktopItem = (makeDesktopItem {
-    name = "Filen";
-    exec = "filen-latest %u";
-    inherit icon;
-    desktopName = "Filen";
-    categories = [ "Applications" ];
-  });
-in
-appimageTools.wrapType2 {
-  pname = "filen";
-  version = "latest";
+  pname = "filen-desktop";
+  version = "2.0.24";
 
   src = fetchurl {
+    # They use an annoying system for latest version url
+    # Means package will need a hash update whenever upstream updates
+    # https://github.com/FilenCloudDienste/filen-desktop/issues/208
     url = "https://cdn.filen.io/desktop/release/filen_x86_64.AppImage";
-    sha256 = "sha256-5vkndT9V/81fUdzS+KTfAjPAGO0IJRx8QhNxBNG8nnU=";
+    hash = "sha256-5vkndT9V/81fUdzS+KTfAjPAGO0IJRx8QhNxBNG8nnU=";
   };
 
+  appimageContents = appimageTools.extractType2 { inherit pname version src; };
+
+  # Needed for the tray to function
+  libPath = lib.makeLibraryPath [ libappindicator-gtk3 ];
+in
+appimageTools.wrapType2 rec {
+  inherit pname version src;
+
   extraInstallCommands = ''
-    mkdir "$out/share"
-    ln -s "${desktopItem}/share/applications" "$out/share/"
+    mv $out/bin/${pname}-${version} $out/bin/${pname}
+
+    source "${makeWrapper}/nix-support/setup-hook"
+    wrapProgram $out/bin/filen-desktop \
+      --prefix LD_LIBRARY_PATH : ${libPath}
+
+    install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
+    substituteInPlace $out/share/applications/${pname}.desktop \
+      --replace 'Exec=AppRun' 'Exec=filen-desktop'
+    cp -r ${appimageContents}/usr/share/icons $out/share
   '';
 
   meta = with lib; {
-    inherit description;
+    description = "Desktop client for Filen.io";
     homepage = "https://filen.io/";
-    license = licenses.gpl3;
-    maintainers = [ ];
+    license = licenses.agpl3Plus;
+    changelog = "https://github.com/FilenCloudDienste/filen-desktop/releases/tag/v${version}";
     platforms = [ "x86_64-linux" ];
   };
 }
