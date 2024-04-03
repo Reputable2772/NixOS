deb8ddb98aac47d97d4f09159ec88ddfa20e064d
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-03 19:07:54 +0530

    feat(appimage): run automatically now
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/Common/Misc/default.nix
+++ System/Common/Misc/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./appimage.nix
     ./sudo.nix
     ./packages.nix
   ];
--- Users/WickedWizard/Programs/Misc/packages.nix
+++ Users/WickedWizard/Programs/Misc/packages.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    appimage-run
     gocryptfs
 
     xwaylandvideobridge
