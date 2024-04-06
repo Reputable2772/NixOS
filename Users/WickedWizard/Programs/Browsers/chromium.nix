e7d8e38e06039a4a9134b77c862c27d7228310ca
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-06 11:28:30 +0530

    fix(cs): remove chromium cs class file
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Browsers/chromium.nix
+++ Users/WickedWizard/Programs/Browsers/chromium.nix
@@ -4,17 +4,4 @@
   home.sessionVariables = {
     CHROME_USER_DATA_DIR = "$HOME/Documents/Browsers/Chromium";
   };
-
-  xdg.desktopEntries = {
-    Computer_Class = {
-      name = "Computer Class";
-      genericName = "Class";
-      exec = ''
-        sh -c "chromium --new-window \"https://app.zoom.us/wc/3095436847/join?from=join&pwd=srgs@omr\" --new-window \"https://app.zoom.us/wc/72459095023/join?from=join&pwd=cs123\" & obs"
-      '';
-      terminal = false;
-      icon = "chromium";
-      type = "Application";
-    };
-  };
 }
