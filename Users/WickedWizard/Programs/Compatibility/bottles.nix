2cc2cca79951f8fd9075e17fd6bb6d277a57e335
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-09 23:58:43 +0530

    fix: override bottles for nixpkgs-wayland
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Compatibility/bottles.nix
+++ Users/WickedWizard/Programs/Compatibility/bottles.nix
@@ -1,3 +1,10 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ bottles ];
+{ pkgs, inputs, ... }: {
+  # This is needed so that Bottles doesn't use the gamescope provided by nix-community/nixpkgs-wayland
+  # and instead uses the one in NixOS/nixpkgs
+  home.packages = with pkgs; [
+    (bottles.override
+      ({
+        bottles-unwrapped = bottles-unwrapped.override ({ gamescope = inputs.nixpkgs.legacyPackages.${pkgs.system}.gamescope; });
+      }))
+  ];
 }
