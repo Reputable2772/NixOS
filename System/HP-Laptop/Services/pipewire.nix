1b558e7285cb47f22528e30a0523b57655570a06
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-11 21:21:51 +0530

    fix: disable pipewire alsa to fix suppressed buffers
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Services/pipewire.nix
+++ System/HP-Laptop/Services/pipewire.nix
@@ -1,9 +1,11 @@
 {
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    # Seems to be an ALSA issue https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/3622
+    # alsa.enable = false;
+    # alsa.support32Bit = f;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber.enable = true;
   };
 }
