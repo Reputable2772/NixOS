b45a7ac60cb3f62eba01c5b105c3968cb815d593
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-13 10:55:54 +0530

    feat: add config module to reduce repititon
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

---
+++ Modules/System/config.nix
@@ -0,0 +1,28 @@
+{ config, lib, ... }:
+with lib;
+let
+  cfg = config.config.config_dir;
+in
+{
+  options.config.config_dir = {
+    config_dir = mkOption {
+      example = literalExpression "$HOME/Documents";
+      type = types.path;
+      description = lib.mdDoc "Directory where all undeclarative config is stored.";
+    };
+    browser_dir = mkOption {
+      default = "${cfg.config_dir}/Browsers";
+      example = literalExpression "\$\{config.users.users.name.home\}/Documents";
+      type = types.path;
+      description = lib.mdDoc "Directory where browsers configuration data is stored.";
+    };
+    self_dir = mkOption {
+      default = "${cfg.config_dir}/NixOS";
+      example = literalExpression "\$\{config.users.users.name.home\}/NixOS";
+      type = types.path;
+      description = lib.mdDoc "Directory where your system flake is stored.";
+    };
+  };
+
+  # config = { };
+}
--- Modules/System/default.nix
+++ Modules/System/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./config.nix
     ./gnome.nix
     ./hyprland.nix
   ];
