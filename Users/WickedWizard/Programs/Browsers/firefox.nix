911173092ecf1e2f7ab381b909f9e0d8252d953c
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-14 13:01:17 +0530

    feat: write absoluteToRelative path parser
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Browsers/firefox.nix
+++ Users/WickedWizard/Programs/Browsers/firefox.nix
@@ -1,19 +1,32 @@
-{ osConfig, pkgs, ... }:
+{ config, osConfig, lib, pkgs, ... }:
 let
   firefox-gnome-theme = pkgs.callPackage ../../../../Packages/firefox-gnome-theme.nix { };
+  # Get path of a with respect to b.
+  # This onlyl works for paths beginning with /home/something/
+  # You can change the numbers for it to work with other paths
+  # The numbers indicate how many parts of the two paths are identical.
+  absoluteToRelative = _a: _b: c:
+    let
+      # Remove trailing '/'
+      a = lib.strings.removeSuffix "/" _a;
+      b = lib.strings.removeSuffix "/" _b;
+
+      # Get length of path b
+      length = lib.lists.length (lib.strings.splitString "/" b) - c;
+      # Drop the paths that are identical in a
+      drop = lib.drop c (lib.strings.splitString "/" (lib.strings.removeSuffix "/" a));
+      path = lib.strings.replicate length "../" + lib.concatMapStrings (s: s + "/") drop;
+    in
+    path;
 in
 {
-  disabledModules = [ "programs/firefox.nix" ];
-
   home.packages = [ firefox-gnome-theme ];
   programs.firefox = {
     enable = true;
     profiles."default" = {
       isDefault = true;
       name = "default";
-      # The custom home manager module suports absolute paths
-      isRelative = 0;
-      path = "${osConfig.programs.config_dir.browser_dir}/Firefox";
+      path = absoluteToRelative "${osConfig.programs.config_dir.browser_dir}/Firefox/" "${config.home.homeDirectory}/.mozilla/firefox/" 3;
       userChrome = ''
         @import "${firefox-gnome-theme}/share/firefox-gnome-theme/userChrome.css";
       '';
