85d9339a91b9451c12f342ad0dc3cf3fc43a5ee2
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-15 10:19:00 +0530

    feat: boot timeout is 3sec
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/boot.nix
+++ System/HP-Laptop/boot.nix
@@ -1,15 +1,22 @@
 { pkgs, ... }:
 {
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernel.sysctl = {
-    "kernel.sysrq" = 1;
-    "net.ipv4.ip_unprivileged_port_start" = 80;
-    "net.core.rmem_max" = 2500000;
-    "net.core.wmem_max" = 2500000;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_zen;
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      timeout = 3;
+    };
+    kernel.sysctl = {
+      "kernel.sysrq" = 1;
+      "net.ipv4.ip_unprivileged_port_start" = 80;
+      "net.core.rmem_max" = 2500000;
+      "net.core.wmem_max" = 2500000;
+    };
+    initrd.kernelModules = [ "amdgpu" ];
+
+    # RTL8821CE
+    extraModulePackages = with pkgs; [ linuxKernel.packages.linux_zen.rtl8821ce ];
+    blacklistedKernelModules = [ "rtw88_8821ce" ];
   };
-  boot.extraModulePackages = with pkgs; [ linuxKernel.packages.linux_zen.rtl8821ce ];
-  boot.blacklistedKernelModules = [ "rtw88_8821ce" ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
 }
