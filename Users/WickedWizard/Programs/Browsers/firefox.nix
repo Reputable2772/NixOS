a79a20045b1e588808c97f41f8c4482a8aa5e7d8
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-16 11:28:10 +0530

    feat: firefox use custom lib
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Browsers/firefox.nix
+++ Users/WickedWizard/Programs/Browsers/firefox.nix
@@ -1,28 +1,6 @@
-{ config, osConfig, lib, pkgs, ... }:
+{ config, osConfig, lib', pkgs, ... }:
 let
   firefox-gnome-theme = pkgs.callPackage ../../../../Packages/firefox-gnome-theme.nix { };
-  # Get path of a with respect to b
-  absoluteToRelative = _a: _b:
-    with lib;
-    let
-      # Remove trailing '/'
-      a = removeSuffix "/" _a;
-      b = removeSuffix "/" _b;
-
-      # Split strings at '/' to form a list
-      splitA = splitString "/" a;
-      splitB = splitString "/" b;
-
-      # Get number of common elements in A and B.
-      c = length (lists.commonPrefix splitA splitB);
-
-      # Number of directories to go back from B to reach common directory for A and B.
-      backLength = length splitB - c;
-      # Get path from common directory.
-      forwardPath = drop c splitA;
-      path = (if backLength < 1 then "./" else "") + strings.replicate backLength "../" + concatMapStrings (s: s + "/") forwardPath;
-    in
-    path;
 in
 {
   home.packages = [ firefox-gnome-theme ];
@@ -31,7 +9,7 @@ in
     profiles."default" = {
       isDefault = true;
       name = "default";
-      path = absoluteToRelative "${osConfig.programs.config_dir.browser_dir}/Firefox/" "${config.home.homeDirectory}/.mozilla/firefox/";
+      path = lib'.absoluteToRelativePath "${osConfig.programs.config_dir.browser_dir}/Firefox/" "${config.home.homeDirectory}/.mozilla/firefox/";
       userChrome = ''
         @import "${firefox-gnome-theme}/share/firefox-gnome-theme/userChrome.css";
       '';
