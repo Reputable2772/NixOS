a4ec434f95589611ffe0f1856d401474ae55cca2
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-16 23:38:08 +0530

    feat: auto pull and upgrade
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Misc/autoupgrade.nix
+++ System/HP-Laptop/Misc/autoupgrade.nix
@@ -1,13 +1,40 @@
-{ config, lib, ... }: {
+{ config, lib, pkgs, ... }: {
+  # Our config is symlinked to /etc/nixos, no matter where it is.
+  # nix.sh/first-time-setup takes care of it.
+
+  systemd.services.pull_flake = {
+    serviceConfig.Type = "oneshot";
+    path = with pkgs; [ git ];
+    script = ''
+      cd /etc/nixos
+      git pull --verbose --autostash --all
+    '';
+
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+  };
+
+  systemd.timers.pull_flake = {
+    wantedBy = [ "timers.target" ];
+    partOf = [ "pull_flake.service" ];
+    timerConfig = {
+      Persistent = true;
+      # Run it 10mins before, to pull flake
+      OnCalendar = "*-*-* 23:50:00 ${config.time.timeZone}";
+      Unit = "pull_flake.service";
+    };
+  };
+
   system.autoUpgrade = {
     enable = true;
     flake = "\"$(readlink -f /etc/nixos)\"";
     flags = [
       "-L"
     ];
-    dates = "daily";
+    dates = "*-*-* 00:00:00 ${config.time.timeZone}";
   };
 
   # We don't need the upgrade flag due to the CI doing it for us. https://github.com/NixOS/nixpkgs/blob/2230a20f2b5a14f2db3d7f13a2dc3c22517e790b/nixos/modules/tasks/auto-upgrade.nix#L255
-  systemd.services.nixos-upgrade.script = lib.mkForce "${config.system.build.nixos-rebuild}/bin/nixos-rebuild ${config.system.autoUpgrade.operation} ${builtins.toString config.system.autoUpgrade.flags}";
+  systemd.services.nixos-upgrade.script =
+    lib.mkForce "${config.system.build.nixos-rebuild}/bin/nixos-rebuild ${config.system.autoUpgrade.operation} ${builtins.toString config.system.autoUpgrade.flags}";
 }
