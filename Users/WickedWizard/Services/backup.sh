9e9d0545db12cadf4a409eef702c8f1b988a13db
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-16 23:45:17 +0530

    fix: remove backup service
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Services/backup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Creating and Syncing Backup"
-/etc/profiles/per-user/wickedwizard/bin/restic --verbose --password-file /home/wickedwizard/Documents/Config/Backup-Key.txt --repo rclone:backup:Backups backup /home/wickedwizard/Documents/
