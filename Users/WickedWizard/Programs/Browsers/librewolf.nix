596132d94048fd77cae8f7925d286bacf008631f
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-16 15:52:06 +0530

    feat: librewolf profile is now declarative
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Browsers/librewolf.nix
+++ Users/WickedWizard/Programs/Browsers/librewolf.nix
@@ -1,4 +1,4 @@
-{
+{ osConfig, ... }: {
   programs.librewolf = {
     enable = true;
     settings = {
@@ -8,6 +8,17 @@
       "privacy.clearOnShutdown.downloads" = false;
     };
   };
+
+  home.file.".librewolf/profiles.ini".text = ''
+    [Profile0]
+    Name=Default
+    IsRelative=0
+    Path=${osConfig.programs.config_dir.browser_dir}/Librewolf
+
+    [General]
+    StartWithLastProfile=1
+    Version=2
+  '';
 
   # programs.autostart.autostartPackages = with pkgs; [ librewolf ];
 
