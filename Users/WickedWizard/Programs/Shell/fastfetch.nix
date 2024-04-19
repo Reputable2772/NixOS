71c418c74619f23dfb2c542d533b9465b66f1986
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-19 21:19:17 +0530

    feat: upgrade fastfetch jsonc hash
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Shell/fastfetch.nix
+++ Users/WickedWizard/Programs/Shell/fastfetch.nix
@@ -6,7 +6,7 @@
     target = "fastfetch/config.jsonc";
     source = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/fastfetch-cli/fastfetch/dev/presets/examples/7.jsonc";
-      hash = "sha256-mJWKbqN0CwSz21d6RHATogMuQxKFmDD3PMD33JiWYL8=";
+      hash = "sha256-WTyW1j3oBAzTXmo2ostgDv2sFG7OPPuQPdCv9rKJ6t0=";
     };
   };
 }
