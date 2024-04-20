d6cef36d869b4f70486bf4d111100ecebac66af7
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-20 15:26:47 +0530

    feat: use substituters correctly for nix daemon
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/Common/configuration.nix
+++ System/Common/configuration.nix
@@ -15,6 +15,20 @@
       auto-optimise-store = true;
       trusted-users = [ "root" "@wheel" ];
       experimental-features = [ "flakes" "nix-command" ];
+      substituters = [
+        "https://spearman4157.cachix.org"
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+        "https://numtide.cachix.org"
+        "https://nixpkgs-wayland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "spearman4157.cachix.org-1:C5rIS9uufmlsdRIe9dEHSZVjxo9g8se+gjMdIeeEYfk="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      ];
     };
   };
 
