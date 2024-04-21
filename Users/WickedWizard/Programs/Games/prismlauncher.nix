98c0f331e078cd211175a70ba8430742bca36c92
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-21 13:30:58 +0530

    feat: prismlauncher uses catppuccin
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Games/prismlauncher.nix
+++ Users/WickedWizard/Programs/Games/prismlauncher.nix
@@ -1,3 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  catppuccin = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "prismlauncher";
+    rev = "07e9c3ca0ff8eb7da9fa8b5329a9d2ceec707f24";
+    sha256 = "sha256-/qUqF92e3kYkl59GUMMFRtRTzVgSp5tGPxnXLGeusys=";
+  };
+in
+{
   home.packages = with pkgs; [ prismlauncher ];
+
+  home.file.".local/share/PrismLauncher/themes/Mocha".source = "${catppuccin}/themes/Mocha";
 }
