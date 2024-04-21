fa62f73408e19e612b6fce363f07350ed72ae715
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-21 13:15:12 +0530

    feat: obs uses catppuccin theme
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Media/obs-studio.nix
+++ Users/WickedWizard/Programs/Media/obs-studio.nix
@@ -1,8 +1,19 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  catppuccin = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "obs";
+    rev = "e7c4fcf387415a20cb747121bc0416c4c8ae3362";
+    sha256 = "sha256-dZcgIPMa1AUFXcMPT99YUUhvxHbniv0Anbh9/DB00NY=";
+  };
+in
+{
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
       obs-pipewire-audio-capture
     ];
   };
+
+  xdg.configFile."obs-studio/themes/".source = "${catppuccin}/themes";
 }
