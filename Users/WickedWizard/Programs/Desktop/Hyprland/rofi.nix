a03204f12888dd9dddff176d04475edbc005b382
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-21 11:55:16 +0530

    feat: install rofi-bluetooth
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Desktop/Hyprland/rofi.nix
+++ Users/WickedWizard/Programs/Desktop/Hyprland/rofi.nix
@@ -5,21 +5,32 @@ in
 {
   home.packages = lib.mkIf hyprland.enable [ pkgs.rofi-wayland ];
 
-  home.file.rofi_theme = {
-    inherit (hyprland) enable;
-    target = ".local/share/rofi/themes/catppuccin-mocha.rasi";
-    source = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/rofi/main/basic/.local/share/rofi/themes/catppuccin-mocha.rasi";
-      hash = "sha256-xths/pnC6WKp3Twc1iqNyjxzep+CRZM47cCjAPpDTxA=";
-    };
-  };
-
   xdg.configFile.rofi = {
     inherit (hyprland) enable;
     target = "rasi/config.rasi";
     source = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/catppuccin/rofi/main/basic/.config/rofi/config.rasi";
       hash = "sha256-+Lyh5Cmd4wdHqx+FuJ8LV2HPpFj4JKT6wCu1xGcLpVg=";
+    };
+  };
+
+  home.file = {
+    rofi_theme = {
+      inherit (hyprland) enable;
+      target = ".local/share/rofi/themes/catppuccin-mocha.rasi";
+      source = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/catppuccin/rofi/main/basic/.local/share/rofi/themes/catppuccin-mocha.rasi";
+        hash = "sha256-xths/pnC6WKp3Twc1iqNyjxzep+CRZM47cCjAPpDTxA=";
+      };
+    };
+    rofi_bluetooth = {
+      inherit (hyprland) enable;
+      target = ".local/bin/rofi-bluetooth";
+      source = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/nickclyde/rofi-bluetooth/master/rofi-bluetooth";
+        hash = "sha256-s06OsedQjd5uDDCVX5h4WPLnMM/OEZzDsCxgQxGojPk=";
+      };
+      executable = true;
     };
   };
 
