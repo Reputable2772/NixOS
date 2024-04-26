91a688f2e8f1a31fed3810351953d81d71f96488
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-26 11:03:08 +0530

    feat: verbose autoupgrade
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Misc/autoupgrade.nix
+++ System/HP-Laptop/Misc/autoupgrade.nix
@@ -7,12 +7,14 @@
     # Beware that you have to pull the changes from CI manually if you use the above.
     flake = "github:Reputable2772/NixOS";
     flags = [
-      "-L"
+      "--print-build-logs"
+      "--show-trace"
     ];
     dates = "*-*-* 00:00:00 ${config.time.timeZone}";
   };
 
   # We don't need the upgrade flag due to the CI doing it for us. https://github.com/NixOS/nixpkgs/blob/2230a20f2b5a14f2db3d7f13a2dc3c22517e790b/nixos/modules/tasks/auto-upgrade.nix#L255
+  # Also note that this doesn't upgrade or downgrade the non-declarative files that are defined in nix.sh/build.
   systemd.services.nixos-upgrade.script =
     lib.mkForce "${config.system.build.nixos-rebuild}/bin/nixos-rebuild ${config.system.autoUpgrade.operation} ${builtins.toString config.system.autoUpgrade.flags}";
 }
