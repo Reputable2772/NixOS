8a3d9c1a941d758812b929f926a5e6125fde24a7
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-28 22:59:40 +0530

    feat: add per-system ssh keys
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

---
+++ System/HP-Laptop/Misc/secrets.nix
@@ -0,0 +1,7 @@
+{
+  programs.secrets = {
+    secrets = {
+      encryption = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID26gevbYFvRTHrv/HwNW09vJhMFP6eOUM8NHOy26V+T wickedwizard@hp-laptop";
+    };
+  };
+}
