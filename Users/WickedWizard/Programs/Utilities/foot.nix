955283d8571101c08319aacaf6860c90f11755c0
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-04-28 18:14:55 +0530

    fix: fix hash and syntax
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Utilities/foot.nix
+++ Users/WickedWizard/Programs/Utilities/foot.nix
@@ -1,11 +1,14 @@
 { pkgs, lib', ... }:
+let
+  catppuccin = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/foot/main/themes/catppuccin-mocha.ini";
+    hash = "sha256-Yr5/n7m3DAwQ8z4EdV9C1BTs88iMvFQ3Ah9L6Dh5hZQ=";
+  };
+in
 {
   programs.foot = {
     enable = true;
-    settings = { } // lib'.iniToNix pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/foot/main/themes/catppuccin-mocha.ini";
-      hash = "";
-    };
+    settings = { } // lib'.iniToNix catppuccin;
   };
 
   wayland.windowManager.hyprland.settings.bind = [
