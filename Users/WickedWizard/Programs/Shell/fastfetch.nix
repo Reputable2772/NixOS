dbc6997b18d761fca7e020cd0fa173cc923cf1de
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-05-04 12:50:41 +0530

    fix: fastfetch config uses nvfetcher
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Shell/fastfetch.nix
+++ Users/WickedWizard/Programs/Shell/fastfetch.nix
@@ -1,12 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs, sources, ... }: {
   home.packages = with pkgs; [ fastfetch ];
 
   xdg.configFile.fastfetch = {
     enable = true;
     target = "fastfetch/config.jsonc";
-    source = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/fastfetch-cli/fastfetch/dev/presets/examples/7.jsonc";
-      hash = "sha256-WTyW1j3oBAzTXmo2ostgDv2sFG7OPPuQPdCv9rKJ6t0=";
-    };
+    source = "${sources.fastfetch.src}/presets/examples/7.jsonc";
   };
 }
