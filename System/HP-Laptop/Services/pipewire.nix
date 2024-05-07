d787c79b0282a22e66d244f8caaa7f06d2f427b8
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-05-07 15:04:57 +0530

    feat: try re-enabling alsa
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Services/pipewire.nix
+++ System/HP-Laptop/Services/pipewire.nix
@@ -2,8 +2,8 @@
   services.pipewire = {
     enable = true;
     # Seems to be an ALSA issue https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/3622
-    # alsa.enable = false;
-    # alsa.support32Bit = f;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
