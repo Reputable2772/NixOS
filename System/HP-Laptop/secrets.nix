7b813120881784d282d59fcdda4fdec22507c0cf
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-05-07 21:31:03 +0530

    fix: remove old secrets.nix
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/secrets.nix
+++
@@ -1,7 +0,0 @@
-let
-  # This should be a passwordless ssh key
-  encryption = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN618WSaf14crbHvqgDdhAqkgjz6tmyjKwL00viq5CQd wickedwizard@hp-laptop";
-in
-{
-  "../../Config/Cachix.age".publicKeys = [ encryption ];
-}
--- Users/WickedWizard/secrets.nix
+++
@@ -1,6 +0,0 @@
-{
-  git = {
-    authorization = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAKg52CA11/TshMFkN689IYepPlIDJZQOA7cMzoe7PU wickedwizard@hp-laptop";
-    signing = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB7s075auCly0MMeG91zc20jjzzp4vm0cz0V8SBGNNpR wickedwizard@hp-laptop";
-  };
-}
