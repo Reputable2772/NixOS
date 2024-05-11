61a5707549f43833fe69d7c4054eb8425ba2e2ed
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-05-11 09:48:00 +0530

    fix: remove bitwarden script to add ssh keys
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Services/scripts.nix
+++ Users/WickedWizard/Services/scripts.nix
@@ -1,13 +1,13 @@
-{ config, config', pkgs, lib, ... }:
+{ pkgs, lib, ... }:
 let
-  script = name: podman: sleep: extra: extra_pkgs: extra_rules: builtins.toString (
+  script = name: podman: sleep: builtins.toString (
     pkgs.resholve.writeScript name
       {
         interpreter = "${lib.getExe pkgs.bash}";
-        inputs = with pkgs; [ podman-compose coreutils ] ++ lib.optionals (builtins.length extra_pkgs > 0) extra_pkgs;
+        inputs = with pkgs; [ podman-compose coreutils ];
         execer = [
           "cannot:${lib.getExe pkgs.podman-compose}"
-        ] ++ lib.optionals (builtins.length extra_rules > 0) extra_rules;
+        ];
       }
       ''
         ${lib.optionalString sleep "sleep 30"}
@@ -20,8 +20,6 @@ let
           podman-compose ${podman}
           cd ..
         done
-
-        ${lib.optionalString (builtins.stringLength extra > 0) extra}
       ''
   );
 in
@@ -32,30 +30,8 @@ in
         Type = "forking";
         Restart = "on-failure";
         RestartSec = 5;
-        ExecStart = script "login.sh" "up -d" true ''
-          export BW_SESSION=$(bw unlock $(ssh-askpass) --raw)
-          export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent
-
-          for folder in $(find ${config'.users.${config.home.username}.config.dir.config}/SSH/ -type f ! -name "*.*"); do
-            key=$(basename $folder)
-            pass=$(bw get item $key | jq ".login.password" | tr -d '"')
-            expect -c "spawn ssh-add $folder" -c "expect \"Enter passphrase\"" -c "send \"$pass\r\"" -c "expect eof"
-          done
-        ''
-          (with pkgs; [
-            bitwarden-cli
-            ssh-askpass-fullscreen
-            jq
-            expect
-            findutils
-          ] ++ [ "${pkgs.gnome.seahorse}/libexec/seahorse/ssh-askpass" ]) [
-          "cannot:${lib.getExe pkgs.expect}"
-          "cannot:${lib.getExe pkgs.bitwarden-cli}"
-          "cannot:${pkgs.gnome.seahorse}/libexec/seahorse/ssh-askpass"
-        ];
-        ExecStop = script "logout.sh" "down" false ''
-          bw lock
-        '' [ pkgs.bitwarden-cli ] [ "cannot:${lib.getExe pkgs.bitwarden-cli}" ];
+        ExecStart = script "login.sh" "up -d" true;
+        ExecStop = script "logout.sh" "down" false;
       };
       Install = {
         WantedBy = [ "graphical-session.target" ];
