a46748cad882b7cd7632bc2b7c1842d9372c55b5
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-05-22 22:05:50 +0530

    fix: remove podman autostart
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Services/scripts.nix
+++ Users/WickedWizard/Services/scripts.nix
@@ -1,30 +1,25 @@
 { config, config', pkgs, lib, ... }:
 let
-  script = name: podman: sleep: extra: extra_pkgs: extra_rules: fake: builtins.toString (
+  script = name: extra: extra_pkgs: execer: fake: builtins.toString (
     pkgs.resholve.writeScript name
       {
-        interpreter = "${lib.getExe pkgs.bash}";
-        inputs = with pkgs; [ podman-compose coreutils ] ++ extra_pkgs;
-        execer = [
-          "cannot:${lib.getExe pkgs.podman-compose}"
-        ] ++ extra_rules;
-        inherit fake;
+        interpreter = lib.getExe pkgs.bash;
+        inputs = with pkgs; [ coreutils ] ++ extra_pkgs;
+        inherit execer fake;
       }
-      ''
-        ${lib.optionalString sleep "sleep 30"}
-
-        cd $HOME/Documents/Applications/Containers
-
-        for folder in *; do
-          echo "In $folder"
-          cd $folder
-          podman-compose ${podman}
-          cd ..
-        done
-
-        ${lib.optionalString (builtins.stringLength extra > 0) extra}
-      ''
+      (lib.optionalString (builtins.stringLength extra > 0) extra)
   );
+  gocryptfs = cmd: lib.optionalString (config'.users.${config.home.username}.mounts ? gocryptfs && config'.users.${config.home.username}.mounts.gocryptfs != null)
+    ''
+      ${lib.pipe
+        config'.users.${config.home.username}.mounts.gocryptfs
+        (with lib; [
+          (attrsets.mapAttrs (n: v: cmd n v))
+          attrsets.attrValues
+          (strings.concatStringsSep "\n")
+        ])
+      }
+    '';
 in
 {
   age.secrets."important-files".file = ../../../Config/important-files.age;
@@ -35,29 +30,15 @@ in
         Type = "forking";
         Restart = "on-failure";
         RestartSec = 5;
-        ExecStart = script "login.sh" "up -d" true
-          (lib.optionalString (config'.users.${config.home.username}.mounts ? gocryptfs && config'.users.${config.home.username}.mounts.gocryptfs != null) ''
-            ${lib.pipe
-              config'.users.${config.home.username}.mounts.gocryptfs
-              (with lib; [
-                (attrsets.mapAttrs (n: v: "gocryptfs ${v.source} ${v.mountpoint} ${optionalString (v ? authentication && v.authentication) "-passfile ${config.age.secrets.${n}.path}"}"))
-                attrsets.attrValues
-                (strings.concatStringsSep "\n")
-              ])
-            }
-          '') [ pkgs.gocryptfs ] [ "cannot:${lib.getExe' pkgs.gocryptfs "gocryptfs"}" ]
+        ExecStart = script "login.sh"
+          (gocryptfs (n: v: "gocryptfs ${v.source} ${v.mountpoint} ${lib.optionalString (v ? authentication && v.authentication) "-passfile ${config.age.secrets.${n}.path}"}"))
+          [ pkgs.gocryptfs ]
+          [ "cannot:${lib.getExe' pkgs.gocryptfs "gocryptfs"}" ]
           { };
-        ExecStop = script "logout.sh" "down" false
-          (lib.optionalString (config'.users.${config.home.username}.mounts ? gocryptfs && config'.users.${config.home.username}.mounts.gocryptfs != null) ''
-            ${lib.pipe
-              config'.users.${config.home.username}.mounts.gocryptfs
-              (with lib; [
-                (attrsets.mapAttrs (n: v: "umount ${v.mountpoint}"))
-                attrsets.attrValues
-                (strings.concatStringsSep "\n")
-              ])
-            }
-          '') [ pkgs.util-linux ] [ ]
+        ExecStop = script "logout.sh"
+          (gocryptfs (n: v: "umount ${v.mountpoint}"))
+          [ pkgs.util-linux ]
+          [ ]
           { external = [ "umount" ]; };
       };
       Install = {
