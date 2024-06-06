a5c620f65a324f693fc5a5f3bdf99992733b2d65
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-06-06 20:35:53 +0530

    feat: override rtl8821ce for every kernel
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Overlays/rtl8821ce.nix
+++ Overlays/rtl8821ce.nix
@@ -1,16 +1,26 @@
-final: prev: {
-  linuxKernel = prev.linuxKernel // {
-    packages = prev.linuxKernel.packages // {
-      linux_zen = prev.linuxKernel.packages.linux_zen // {
-        rtl8821ce = prev.linuxKernel.packages.linux_zen.rtl8821ce.overrideAttrs {
-          src = prev.fetchFromGitHub {
-            owner = "tomaspinho";
-            repo = "rtl8821ce";
-            rev = "f119398d868b1a3395f40c1df2e08b57b2c882cd";
-            hash = "sha256-EfpKa5ZRBVM5T8EVim3cVX1PP1UM9CyG6tN5Br8zYww=";
+final: prev:
+let
+  inherit (prev) lib linuxKernel;
+  inherit (lib.attrsets) filterAttrs mapAttrs;
+  inherit (lib.strings) hasPrefix;
+  inherit (lib.trivial) pipe;
+in
+{
+  linuxKernel = linuxKernel // {
+    packages = linuxKernel.packages //
+      (pipe linuxKernel.packages [
+        # Get every attrName that begins with linux and doesn't throw an error for value
+        (filterAttrs (n: v: hasPrefix "linux" n && (builtins.tryEval v).success))
+        (mapAttrs (n: v: v // {
+          rtl8821ce = linuxKernel.packages.${n}.rtl8821ce.overrideAttrs {
+            src = prev.fetchFromGitHub {
+              owner = "tomaspinho";
+              repo = "rtl8821ce";
+              rev = "f119398d868b1a3395f40c1df2e08b57b2c882cd";
+              hash = "sha256-EfpKa5ZRBVM5T8EVim3cVX1PP1UM9CyG6tN5Br8zYww=";
+            };
           };
-        };
-      };
-    };
+        }))
+      ]);
   };
 }
