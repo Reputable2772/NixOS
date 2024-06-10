4edf8a01885acc8d2ebdda9562a0b4441e358e3f
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-06-10 21:56:15 +0530

    feat: calibre use catppuccin icons
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Media/calibre.nix
+++ Users/WickedWizard/Programs/Media/calibre.nix
@@ -1,3 +1,15 @@
-{ pkgs, ... }: {
+{ pkgs, sources, ... }:
+let
+  catppuccin-calibre = (pkgs.callPackage ../../../../Packages/calibre-catppuccin.nix { inherit sources; });
+in
+{
   home.packages = with pkgs; [ calibre ];
+
+  xdg.configFile."calibre/resources/images".source = catppuccin-calibre.mocha.outPath;
+  /**
+    TODO: Use home.activation to check whether the JSON of ~/.config/calibre/gui.json has
+    the JSON contained by the theme files.
+
+    Currently too much effort.
+   */
 }
