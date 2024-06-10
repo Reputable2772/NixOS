a3a556fed96b67d1083b4ba5e7d869aae514fb8e
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-06-10 14:47:50 +0530

    fix: use full paths for hypridle
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Desktop/Hyprland/hypridle.nix
+++ Users/WickedWizard/Programs/Desktop/Hyprland/hypridle.nix
@@ -1,31 +1,31 @@
-{ config, ... }: {
+{ config, pkgs, lib, ... }: {
   services.hypridle = {
     enable = config.wayland.windowManager.hyprland.enable;
     settings = {
       general = {
-        lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch dpms on";
+        lock_cmd = "${pkgs.procps}/bin/pidof hyprlock || ${lib.getExe pkgs.hyprlock}";
+        before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";
+        after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
       };
 
       listener = [
         {
           timeout = 210;
-          on-timeout = "brightnessctl -s set 10";
-          on-resume = "brightnessctl -r";
+          on-timeout = "${lib.getExe pkgs.brightnessctl} -s set 10";
+          on-resume = "${lib.getExe pkgs.brightnessctl} -r";
         }
         {
           timeout = 240;
-          on-timeout = "loginctl lock-session";
+          on-timeout = "${pkgs.systemd}/bin/loginctl lock-session";
         }
         {
           timeout = 270;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
+          on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
+          on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
         }
         {
           timeout = 300;
-          on-timeout = "systemctl suspend";
+          on-timeout = "${pkgs.systemd}/bin/systemctl suspend";
         }
       ];
     };
