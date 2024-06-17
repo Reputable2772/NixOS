7a5fead7685761222e30ccf2b28587df8525c49a
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-06-17 22:24:18 +0530

    fix: set order of path correctly, ignoring nixpkgs hyprland module
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Modules/System/hyprland.nix
+++ Modules/System/hyprland.nix
@@ -13,7 +13,7 @@ in
   config = mkIf cfg.enable {
     # Fixes https://github.com/NixOS/nixpkgs/issues/189851#issuecomment-1238907955
     systemd.user.extraConfig = ''
-      DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+      DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin:$PATH"
     '';
 
     xdg.portal = {
