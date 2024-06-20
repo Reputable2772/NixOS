e38d8a31ffa754ceb66c6d3b31aecfa32de69349
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-06-20 22:54:39 +0530

    feat: move nix.sh commands to devshell and modify changelog
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Shells/default.nix
+++ Shells/default.nix
@@ -12,6 +12,7 @@ pkgs.devshell.mkShell {
       jq
       hydra-check
       nixpkgs-fmt
+      nix-diff
       inputs.agenix.packages.${pkgs.system}.default
     ];
   };
@@ -24,4 +25,84 @@ pkgs.devshell.mkShell {
       ${lib.getExe pkgs.commitizen} check --allow-abort --commit-msg-file $1
     '';
   };
+
+  commands = [
+    {
+      help = "Run `nixos-rebuild --flake . --fast test` to test the current config.";
+      name = "test-config";
+      command = "sudo nixos-rebuild --flake . --fast test";
+      category = "Development";
+    }
+    {
+      help = "Garbage collect generations > 2 days old";
+      name = "gc";
+      command = ''
+        sudo nix-collect-garbage --delete-older-than 2d
+        nix-collect-garbage --delete-older-than 2d
+      '';
+    }
+    {
+      help = "Format files";
+      name = "fmt";
+      command = "nix fmt";
+    }
+    {
+      help = "Build and switch to a new configuration";
+      name = "switch";
+      command = ''
+        echo "Copying non-declarative files"
+
+        # If file is not a symlink, then copy it over if it is new.
+        if [[ ! -L "$HOME/.config/mimeapps.list" ]]; then
+          cp -pu "$HOME/.config/mimeapps.list" "$PWD/Config/mimeapps.list"
+        fi
+
+        echo "Builiding"
+        sudo nixos-rebuild switch --flake .
+      '';
+    }
+    {
+      help = "Check Flakes";
+      name = "check";
+      command = "nix flake check";
+    }
+    {
+      help = "Show changelog with the previous revision.";
+      name = "changelog";
+      command = ''
+        if [ -z $@ ]; then
+          gen=$(nixos-rebuild list-generations --json | jq '.[] | select(.current == true) | .generation')
+        else
+          gen=$(echo $@)
+        fi
+
+        prev_gen=$(expr $gen - 1)
+        nix-diff $(nix-store -qd "/nix/var/nix/profiles/system-$gen-link" "/nix/var/nix/profiles/system-$prev_gen-link")
+      '';
+    }
+    {
+      help = "Tell us exactly how our flake depends on a certain derivation";
+      name = "why-depends";
+      command = "NIXPKGS_ALLOW_INSECURE=1 NIXPKGS_ALLOW_UNFREE=1 nix why-depends --impure --derivation .#nixosConfigurations.$(hostname).config.system.build.toplevel .#nixosConfigurations.$(hostname).$1";
+    }
+    {
+      help = "Tell when a package last built successfully.";
+      name = "last-successful-build";
+      command = ''
+        # Taken from here # https://discourse.nixos.org/t/how-to-get-the-latest-unbroken-commit-for-a-broken-package-from-hydra/26354
+
+        package=$1
+        hydraURL=$(hydra-check --json "$package" | jq ".\"$package\" | .[0].build_url" -r)
+
+        echo "Hydra URL: $hydraURL"
+
+        evalID=$(curl -sH 'Accept: application/json' "$hydraURL" | jq '.jobsetevals | .[0]')
+        eval=$(curl -sH 'Accept: application/json' https://hydra.nixos.org/eval/"$evalID")
+        rev=$(echo "$eval" | jq '.jobsetevalinputs.nixpkgs.revision')
+        evalDate=$(date --date @"$(echo "$eval" | jq '.timestamp')")
+
+        echo "Last working Nixpkgs Revision: $rev, evaluated at $evalDate"
+      '';
+    }
+  ];
 }
--- nix.sh
+++ nix.sh
@@ -2,28 +2,6 @@
 
 set -e
 
-build() {
-	echo "Copying non-declarative files"
-
-	# If file is not a symlink, then copy it over if it is new.
-	if [[ ! -L "$HOME/.config/mimeapps.list" ]]; then
-		cp -pu "$HOME/.config/mimeapps.list" "$PWD/Config/mimeapps.list"
-	fi
-
-	echo "Builiding"
-	sudo nixos-rebuild switch --flake .
-}
-
-changelog() {
-	echo "Changelog"
-	nix profile diff-closures --profile /nix/var/nix/profiles/system
-}
-
-check() {
-	echo "Checking Flake config"
-	nix flake check
-}
-
 ci() {
 	# large docker images
     sudo docker image prune --all --force
@@ -58,12 +36,6 @@ ci_get() {
 	esac
 }
 
-clean() {
-	sudo nix-collect-garbage --delete-older-than 2d
-	nix-collect-garbage --delete-older-than 2d
-	# sudo nixos-rebuild boot --flake .#hp-laptop
-}
-
 dconf_nix() {
 	echo "Dumping."
 	dconf dump / | tee wickedwizard.dconf
@@ -90,16 +62,6 @@ dconf_nix() {
 	rm -rf *.conf *.dconf
 }
 
-depends() {
-	echo "The nix derivation depends on $1 because: "
-	NIXPKGS_ALLOW_INSECURE=1 NIXPKGS_ALLOW_UNFREE=1 nix why-depends --impure --derivation .#nixosConfigurations.$(hostname).config.system.build.toplevel .#nixosConfigurations.$(hostname).pkgs.$1
-}
-
-format() {
-	echo "Formatting files."
-	nix fmt
-}
-
 first_time_setup() {
 	echo "Modify Config/config.nix, and then hit enter."
 	read
@@ -118,47 +80,17 @@ first_time_setup() {
 	echo "Setup completed."
 }
 
-last_unbroken() {
-	# Taken from here # https://discourse.nixos.org/t/how-to-get-the-latest-unbroken-commit-for-a-broken-package-from-hydra/26354
-
-	package=$1
-	hydraURL=$(hydra-check --json "$package" | jq ".\"$package\" | .[0].build_url" -r)
-
-	echo "Hydra URL: $hydraURL"
-
-	evalID=$(curl -sH 'Accept: application/json' "$hydraURL" | jq '.jobsetevals | .[0]')
-	eval=$(curl -sH 'Accept: application/json' https://hydra.nixos.org/eval/"$evalID")
-	rev=$(echo "$eval" | jq '.jobsetevalinputs.nixpkgs.revision')
-	evalDate=$(date --date @"$(echo "$eval" | jq '.timestamp')")
-
-	echo "Last working Nixpkgs Revision: $rev, evaluated at $evalDate"
-}
-
 case $1 in
-	"build")
-		build;;
-	"changelog")
-		changelog;;
-	"check")
-		check;;
 	"ci")
 		ci;;
-	"clean")
-		clean;;
 	"dconf")
 		dconf_nix;;
-	"format")
-		format;;
 	"first-time-setup")
 		first_time_setup;;
-	"why-depends")
-		depends $2;;
-	"last-unbroken")
-		last_unbroken $2;;
 
 	# Meant for CI to get trusted-public-keys or trusted-substituters
 	"ci_get")
 		ci_get $2;;
 	*)
-		echo "Invalid option. Expected 'build', 'changelog', 'check', 'ci', 'dconf', 'format', 'first-time-setup', 'why-depends' or 'last-unbroken'";;
+		echo "Invalid option. Expected 'ci', 'dconf' or 'first-time-setup'";;
 esac
