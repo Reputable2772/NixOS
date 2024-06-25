05e8e21f60123a8c74a1d89fe1553ea47ce3fb2c
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-06-25 14:28:21 +0530

    feat: nix-ld use packages from all users
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/Common/Misc/nix-ld-rs.nix
+++ System/Common/Misc/nix-ld-rs.nix
@@ -1,63 +1,20 @@
-{ pkgs, inputs, ... }:
+{ config, pkgs, inputs, lib, ... }:
+let
+  inherit (lib.attrsets) attrValues filterAttrs mapAttrs;
+  inherit (lib.lists) concatLists;
+  inherit (lib.trivial) pipe;
+in
 {
-  # Enable nix ld
   programs.nix-ld = {
     enable = true;
     package = inputs.nix-ld-rs.packages.${pkgs.system}.nix-ld-rs;
 
-    # Taken from https://github.com/Mic92/dotfiles/blob/main/nixos/modules/nix-ld.nix
-    libraries = with pkgs; [
-      alsa-lib
-      at-spi2-atk
-      at-spi2-core
-      atk
-      cairo
-      cups
-      curl
-      dbus
-      expat
-      fontconfig
-      freetype
-      fuse3
-      gdk-pixbuf
-      glib
-      gtk3
-      icu
-      libGL
-      libappindicator-gtk3
-      libdrm
-      libglvnd
-      libnotify
-      libpulseaudio
-      libunwind
-      libusb1
-      libuuid
-      libxkbcommon
-      libxml2
-      mesa
-      nspr
-      nss
-      openssl
-      pango
-      pipewire
-      stdenv.cc.cc
-      systemd
-      vulkan-loader
-      xorg.libX11
-      xorg.libXScrnSaver
-      xorg.libXcomposite
-      xorg.libXcursor
-      xorg.libXdamage
-      xorg.libXext
-      xorg.libXfixes
-      xorg.libXi
-      xorg.libXrandr
-      xorg.libXrender
-      xorg.libXtst
-      xorg.libxcb
-      xorg.libxkbfile
-      xorg.libxshmfence
-      zlib
+    # All packages installed by any user that has HM.
+    libraries = pipe config.users.users [
+      (filterAttrs (n: v: v.isNormalUser))
+      (mapAttrs (n: v: config.home-manager.users.${n}.home.packages))
+      attrValues
+      concatLists
     ];
   };
 }
