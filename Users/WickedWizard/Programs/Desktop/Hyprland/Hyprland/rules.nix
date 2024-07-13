c7acecf75b3fa4bc5b26e6010d748432a5a1f0cf
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-07-13 14:00:16 +0530

    feat: inhibit idle on fullscreen & xwaylandvideobridge window rules
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Desktop/Hyprland/Hyprland/rules.nix
+++ Users/WickedWizard/Programs/Desktop/Hyprland/Hyprland/rules.nix
@@ -1,11 +1,10 @@
 {
   wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
-      "opacity 0.0 override 0.0 override,class:^(xwaylandvideobridge)$"
-      "noanim,class:^(xwaylandvideobridge)$"
-      "noinitialfocus,class:^(xwaylandvideobridge)$"
-      "maxsize 1 1,class:^(xwaylandvideobridge)$"
-      "noblur,class:^(xwaylandvideobridge)$"
+      # Fullscreen should inhibit idle
+      "idleinhibit fullscreen, class:^(*)$"
+      "idleinhibit fullscreen, title:^(*)$"
+      "idleinhibit fullscreen, fullscreen:1"
     ];
   };
 }
