856d10f5fc018022e75e5c9d150fa2ee2427e55c
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-07-25 19:15:36 +0530

    feat: backup games with ludusavi
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Backup/backup.nix
+++ Users/WickedWizard/Programs/Backup/backup.nix
@@ -1,9 +1,13 @@
-{ config', ... }: {
+{ config', pkgs, lib, ... }: {
   programs.backup = {
     inherit (config'.backup) repository;
     paths = {
       inherit (config'.backup.paths) include exclude;
     };
     enable = true;
+    preBackupScript = ''
+      # Config for ludusavi should be set by ludusavi itself.
+      ${lib.getExe pkgs.ludusavi} backup --force
+    '';
   };
 }
