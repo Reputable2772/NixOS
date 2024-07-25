7217c6e52e0528025d1f779d930e3f136677138d
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-07-25 22:13:19 +0530

    feat: add ludusavi config
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Config/config.nix
+++ Config/config.nix
@@ -38,13 +38,15 @@ rec {
 
   users = {
     wickedwizard = rec {
+      home = "/home/wickedwizard";
       /**
         There needs to be a corresponding age file, with the name
         `user-backup.age`, use system encryption keys only.
        */
       backup = rec {
         # Internal variables
-        base = "/home/wickedwizard/Documents";
+        base = "${home}/Documents";
+        games = "${base}/Games";
 
         repository = "${base}/Backup";
         paths = {
@@ -61,6 +63,100 @@ rec {
             "Joplin"
           ];
           exclude = [ ];
+        };
+        # Ludusavi is a backup tool for Games.
+        ludusavi = {
+          backup = {
+            filter = {
+              cloud = {
+                epic = false;
+                exclude = false;
+                gog = false;
+                origin = false;
+                steam = false;
+                uplay = false;
+              };
+              excludeStoreScreenshots = false;
+              ignoredPaths = [ ];
+              ignoredRegistry = [ ];
+            };
+            format = {
+              chosen = "zip";
+              compression = {
+                bzip2.level = 6;
+                deflate.level = 6;
+                zstd.level = 10;
+              };
+              zip.compression = "zstd";
+            };
+            ignoredGames = [ ];
+            path = "${games}/Saves";
+            retention = {
+              differential = 0;
+              full = 1;
+            };
+            sort = {
+              key = "name";
+              reversed = false;
+            };
+            toggledPaths = { };
+            toggledRegistry = { };
+          };
+          customGames = [
+            {
+              files = [
+                "${games}/Linux/Half-Life/game/valve/*.cfg"
+                "${games}/Linux/Half-Life/game/valve/SAVE"
+              ];
+              name = "Half-Life";
+              registry = [ ];
+            }
+            {
+              files = [
+                "${games}/Windows/Half-Life 2/drive_c/Program Files (x86)/DODI-Repacks/Half Life 2/hl2/cfg"
+                "${games}/Windows/Half-Life 2/drive_c/Program Files (x86)/DODI-Repacks/Half Life 2/hl2/save"
+              ];
+              name = "Half-Life 2";
+              registry = [ ];
+            }
+            {
+              files = [
+                "${games}/Linux/Factorio/*.dat"
+                "${games}/Linux/Factorio/config"
+                "${games}/Linux/Factorio/saves"
+                "${home}/.factorio/config"
+                "${home}/.factorio/saves"
+              ];
+              name = "Factorio";
+              registry = [ ];
+            }
+          ];
+          language = "en-US";
+          manifest.url = "https://raw.githubusercontent.com/mtkennerly/ludusavi-manifest/master/data/manifest.yaml";
+          redirects = [ ];
+          restore = {
+            ignoredGames = [ ];
+            path = "${games}/Saves";
+            sort = {
+              key = "status";
+              reversed = false;
+            };
+            toggledPaths = { };
+            toggledRegistry = { };
+          };
+          roots = [
+            { path = "${home}/.local/share/Steam"; store = "steam"; }
+            { path = "${home}/.config/heroic"; store = "heroic"; }
+            { path = "${games}/Windows/*"; store = "otherWine"; }
+            { path = "${games}/Linux/*"; store = "otherLinux"; }
+          ];
+          runtime.threads = null;
+          scan = {
+            showDeselectedGames = true;
+            showUnchangedGames = true;
+            showUnscannedGames = true;
+          };
+          theme = "dark";
         };
       };
 
@@ -87,7 +183,7 @@ rec {
       };
       dir = rec {
         # Internal variable, should not be used
-        base = "/home/wickedwizard/Documents";
+        base = "${home}/Documents";
         # Where to store the browser profiles, for easily moving it about.
         browsers = "${base}/Browsers";
         # Where to store generic undeclarative user config. Should be gitignored
--- Users/WickedWizard/Programs/Games/ludusavi.nix
+++ Users/WickedWizard/Programs/Games/ludusavi.nix
@@ -1,3 +1,5 @@
-{ pkgs, ... }: {
+{ config', pkgs, ... }: {
   home.packages = with pkgs; [ ludusavi ];
+
+  xdg.configFile."ludusavi/config.yaml".source = (pkgs.formats.yaml { }).generate "ludusavi-config" config'.backup.ludusavi;
 }
