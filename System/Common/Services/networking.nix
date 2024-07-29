c7876a0b22c8d3fab2d010ae7d80e4fcceb1ed13
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-07-29 16:51:59 +0530

    feat: malware blocking cloudflare
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/Common/Services/networking.nix
+++ System/Common/Services/networking.nix
@@ -6,8 +6,8 @@
     };
 
     nameservers = [
-      "1.1.1.1#cloudflare-dns.com"
-      "1.0.0.1#cloudflare-dns.com"
+      "1.1.1.3#cloudflare-dns.com"
+      "1.0.0.3#cloudflare-dns.com"
       "2606:4700:4700::1111#cloudflare-dns.com"
       "2606:4700:4700::1001#cloudflare-dns.com"
     ];
