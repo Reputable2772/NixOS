2bf0b7874091c175851f331b780003d63849252f
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-09-03 10:05:26 +0530

    feat: enable hm gnome services
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Services/keyring.nix
+++ System/HP-Laptop/Services/keyring.nix
@@ -1,3 +1,5 @@
-{
+{ config, ... }: {
   services.gnome.gnome-keyring.enable = true;
+
+  home-manager.sharedModules = [{ services.gnome-keyring.enable = config.services.gnome.gnome-keyring.enable; }];
 }
