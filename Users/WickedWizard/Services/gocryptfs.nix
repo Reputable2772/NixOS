246e3fdd8aca4d0a23d2a8c8185bc37ef78d6537
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-09-04 21:54:21 +0530

    chore: rewrite gocryptfs service
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Services/gocryptfs.nix
+++ Users/WickedWizard/Services/gocryptfs.nix
@@ -1,6 +1,6 @@
 { config, config', pkgs, lib, ... }:
 let
-  inherit (lib.attrsets) mapAttrs attrValues filterAttrs concatMapAttrs;
+  inherit (lib.attrsets) filterAttrs mapAttrs' mapAttrsToList nameValuePair;
   inherit (lib.strings) concatStringsSep;
   inherit (lib.trivial) pipe;
   script = name: script: extra_pkgs: execer: fake: (
@@ -15,51 +15,44 @@ let
   validMounts = _pipe: lib.mkIf
     (config'.mounts ? gocryptfs && config'.mounts.gocryptfs != null)
     (pipe config'.mounts.gocryptfs _pipe);
+  map_cmd = cmd: (validMounts [
+    (mapAttrsToList cmd)
+    (concatStringsSep "\n")
+  ]).content;
 in
 {
   age.secrets = validMounts [
     (filterAttrs (n: v: v ? authentication && v.authentication))
-    (concatMapAttrs (n: v: {
-      ${n}.file = ./. + "../../../../Config/${n}.age";
-    }))
+    (mapAttrs' (n: v: nameValuePair n { file = ./. + "../../../../Config/${n}.age"; }))
   ];
 
-  systemd.user.services.gocryptfs =
-    let
-      gocryptfs_map = cmd:
-        (validMounts [
-          (mapAttrs cmd)
-          attrValues
-          (concatStringsSep "\n")
-        ]).content;
-    in
-    {
-      Service = {
-        Type = "forking";
-        Restart = "on-failure";
-        RestartSec = 5;
-        ExecStartPre = script "pre-login.sh"
-          (gocryptfs_map (n: v: "mkdir -p ${v.mountpoint}"))
-          [ ]
-          [ ]
-          { };
+  systemd.user.services.gocryptfs = {
+    Service = {
+      Type = "forking";
+      Restart = "on-failure";
+      RestartSec = 5;
+      ExecStartPre = script "gocryptfs-premount.sh"
+        (map_cmd (n: v: "mkdir -p ${v.mountpoint}"))
+        [ ]
+        [ ]
+        { };
 
-        ExecStart = script "login.sh"
-          (gocryptfs_map (n: v: "gocryptfs ${v.source} ${v.mountpoint} ${lib.optionalString (v ? authentication && v.authentication) "-passfile ${config.age.secrets.${n}.path}"}"))
-          [ pkgs.gocryptfs ]
-          [ "cannot:${lib.getExe' pkgs.gocryptfs "gocryptfs"}" ]
-          { };
-        ExecStop = script "logout.sh"
-          (gocryptfs_map (n: v: "fusermount -u ${v.mountpoint}"))
-          [ pkgs.fuse ]
-          [ ]
-          { external = [ "fusermount" ]; };
-        # Force gocryptfs and others to use the fusermount wrapper in /run/wrappers/bin, otherwise permissions error occurs.
-        Environment = [ "PATH=/run/wrappers/bin" ];
-      };
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
+      ExecStart = script "gocryptfs-mount.sh"
+        (map_cmd (n: v: "gocryptfs ${v.source} ${v.mountpoint} ${lib.optionalString (v ? authentication && v.authentication) "-passfile ${config.age.secrets.${n}.path}"}"))
+        [ pkgs.gocryptfs ]
+        [ "cannot:${lib.getExe' pkgs.gocryptfs "gocryptfs"}" ]
+        { };
+
+      ExecStop = script "gocryptfs-unmount.sh"
+        (map_cmd (n: v: "fusermount -u ${v.mountpoint}"))
+        [ pkgs.fuse ]
+        [ ]
+        { external = [ "fusermount" ]; };
+
+      # Force gocryptfs and others to use the fusermount wrapper in /run/wrappers/bin, otherwise permissions error occurs.
+      Environment = [ "PATH=/run/wrappers/bin" ];
     };
+    Install.WantedBy = [ "default.target" ];
+  };
 }
 
