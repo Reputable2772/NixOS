c13e0d185d1342340dfd07253870c6302b057f44
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-09-10 22:08:17 +0530

    feat: change networking nameservers
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/Common/Services/networking.nix
+++ System/Common/Services/networking.nix
@@ -6,8 +6,8 @@
     };
 
     nameservers = [
-      "1.1.1.3#cloudflare-dns.com"
-      "1.0.0.3#cloudflare-dns.com"
+      "1.1.1.1#cloudflare-dns.com"
+      "1.0.0.1#cloudflare-dns.com"
       "2606:4700:4700::1111#cloudflare-dns.com"
       "2606:4700:4700::1001#cloudflare-dns.com"
     ];
@@ -19,12 +19,12 @@
     dnssec = "false";
     llmnr = "true";
     domains = [ "~." ];
+    dnsovertls = "true";
     fallbackDns = [
       "8.8.8.8"
       "2001:4860:4860::8844"
     ];
     extraConfig = ''
-      DNSOverTLS=true
       MulticastDNS=resolve
       DNSStubListenerExtra=0.0.0.0
     '';
