3a1f97ed9690c7e8372c4fb43f38289100052e68
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-09-15 18:17:08 +0530

    feat: lutris fhsenv use steam-run packages
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Games/lutris.nix
+++ Users/WickedWizard/Programs/Games/lutris.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    lutris
+    (lutris.override { extraLibraries = pkgs.steam-run.fhsenv.args.multiPkgs; })
 
     # Fixes NixOS/nixpkgs#285748
     winetricks
