1260aa2298e5e8b9bb2d651dc27fb06ef0e41553
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-09-21 10:47:42 +0530

    feat: hibernate (instead of suspend) devices on low power
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/Common/Misc/udev.nix
+++ System/Common/Misc/udev.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 {
   services.udev.extraRules = ''
-    # Suspend's device on Low Power. Set 20 to be your low power value, in percent.
-    SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="${toString 20}", RUN+="${config.systemd.package}/bin/systemctl suspend"
+    # Hibernate's device on Low Power. Set 20 to be your low power value, in percent.
+    SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="${toString 20}", RUN+="${config.systemd.package}/bin/systemctl hibernate"
   '';
 }
