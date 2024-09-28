207a727b4aeaf0c67184faacff7a4a2bc1346b20
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-09-28 11:14:40 +0530

    feat: lutris don't make winetricks global
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Games/lutris.nix
+++ Users/WickedWizard/Programs/Games/lutris.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     (lutris.override {
-      extraLibraries = pkgs.steam-run.fhsenv.args.multiPkgs;
+      extraLibraries = steam-run.fhsenv.args.multiPkgs;
+      # Fixes NixOS/nixpkgs#285748
+      extraPkgs = pkgz: with pkgz; [ winetricks ];
+      # Since steam is a flatpak.
       steamSupport = false;
     })
-
-    # Fixes NixOS/nixpkgs#285748
-    winetricks
   ];
 }
