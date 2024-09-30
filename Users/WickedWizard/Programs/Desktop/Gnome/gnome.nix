457b083c6c33e66149e5615ecb0649be57775c3a
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-09-30 21:43:42 +0530

    feat: setup gnome settings
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Desktop/Gnome/gnome.nix
+++ Users/WickedWizard/Programs/Desktop/Gnome/gnome.nix
@@ -10,13 +10,29 @@ in
       dconf-editor
     ] ++ lib.optionals cond (with pkgs.gnomeExtensions; [
       appindicator
-      bluetooth-battery
       caffeine
-      just-perfection
+
+      # Can stay non-declarative, since all bluetooth devices configuration is non-declarative anyway.
+      bluetooth-battery
+      # Can be used once oae/gnome-shell-pano#271 is closed.
       pano
-      rounded-window-corners
-      vitals
     ]);
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      disabled-extensions = [ "auto-move-windows@gnome-shell-extensions.gcampax.github.com" "apps-menu@gnome-shell-extensions.gcampax.github.com" "window-list@gnome-shell-extensions.gcampax.github.com" "workspace-indicator@gnome-shell-extensions.gcampax.github.com" "native-window-placement@gnome-shell-extensions.gcampax.github.com" "just-perfection-desktop@just-perfection" "Vitals@CoreCoding.com" "drive-menu@gnome-shell-extensions.gcampax.github.com" ];
+      enabled-extensions = [ "appindicatorsupport@rgcjonas.gmail.com" "bluetooth-battery@michalw.github.com" "caffeine@patapon.info" "launch-new-instance@gnome-shell-extensions.gcampax.github.com" "pano@elhan.io" "screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com" "user-theme@gnome-shell-extensions.gcampax.github.com" "system-monitor@gnome-shell-extensions.gcampax.github.com" ];
+      favorite-apps = [ "librewolf.desktop" "firefox.desktop" "org.codeberg.dnkl.foot.desktop" "@joplinapp-desktop.desktop" ];
+    };
+
+    "org/gnome/shell/extensions/caffeine" = {
+      indicator-position-max = 3;
+    };
+
+    "org/gnome/shell/extensions/system-monitor" = {
+      show-swap = false;
+    };
+  };
 
   # Fixes NixOS/nixpkgs#53631
   home.sessionVariables = lib.optionalAttrs cond {
