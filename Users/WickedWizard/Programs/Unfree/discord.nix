ba41a11029ec8010f3e158826410d1bce372ce65
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-10-01 20:56:24 +0530

    feat: switch to vesktop flatpak
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Unfree/discord.nix
+++ Users/WickedWizard/Programs/Unfree/discord.nix
@@ -1,7 +1,5 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    vesktop
+{
+  services.flatpak.packages = [
+    "dev.vencord.Vesktop"
   ];
-
-  # programs.autostart.packages = with pkgs; [ vesktop ];
 }
