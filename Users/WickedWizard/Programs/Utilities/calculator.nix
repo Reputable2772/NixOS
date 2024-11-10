a8079fc8d3c56587e90f971680fba5126b6f9b87
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-11-10 16:53:30 +0530

    fix: install qalculator only for hyprland
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Utilities/calculator.nix
+++ Users/WickedWizard/Programs/Utilities/calculator.nix
@@ -1,5 +1,5 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ qalculate-gtk ];
+{ config, pkgs, lib, ... }: {
+  home.packages = lib.optional config.wayland.windowManager.hyprland.enable pkgs.qalculate-gtk;
 
   wayland.windowManager.hyprland.settings.bind = [
     ", XF86Calculator, exec, qalculate-gtk"
