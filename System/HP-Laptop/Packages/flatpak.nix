4c6a1d4b6f4664a565be3b4eda257eeb42feefa6
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-11-10 16:52:21 +0530

    fix: disallow system flatpaks
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Packages/flatpak.nix
+++ System/HP-Laptop/Packages/flatpak.nix
@@ -1,21 +1,3 @@
-{ inputs, ... }: {
-  services.flatpak = {
-    enable = true;
-    update.onActivation = true;
-    uninstallUnmanaged = true;
-    remotes = [
-      {
-        name = "flathub";
-        location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-      }
-      {
-        name = "flathub-beta";
-        location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
-      }
-    ];
-  };
-
-  imports = [
-    inputs.nix-flatpak.nixosModules.nix-flatpak
-  ];
+{
+  services.flatpak.enable = true;
 }
