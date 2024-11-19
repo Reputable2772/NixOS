c1c609448f8a38c31e476b0f2335f882381b843d
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-11-19 19:36:08 +0530

    feat: switch back to hyprland
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Packages/gnome.nix
+++ System/HP-Laptop/Packages/gnome.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
-  services.xserver.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.enable = false;
+  services.xserver.desktopManager.gnome.enable = false;
+  services.xserver.displayManager.gdm.enable = false;
 
   environment.gnome.excludePackages = with pkgs; [
     gnome-photos
