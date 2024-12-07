e4fb31538a6edb257185f41ad6a1480451c60d2c
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-12-07 18:53:48 +0530

    feat: add misc windowrules
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Desktop/Hyprland/Hypr/rules.nix
+++ Users/WickedWizard/Programs/Desktop/Hyprland/Hypr/rules.nix
@@ -5,6 +5,26 @@
       "idleinhibit fullscreen, class:^(*)$"
       "idleinhibit fullscreen, title:^(*)$"
       "idleinhibit fullscreen, fullscreen:1"
+
+      # Picture-in-Picture for any windows tagged pip
+      "float, tag:pip"
+      "pin, tag:pip"
+      "keepaspectratio, tag:pip"
+      "noborder, tag:pip"
+      "plugin:hyprbars:nobar, tag:pip"
+      "size 480 270, tag:pip"
+      "minsize 240 135, tag:pip"
+      "maxsize 960 540, tag:pip"
+      "move 100%-490 100%-280, tag:pip"
+
+      # make pop-up file dialogs floating, centred, and pinned
+      "tag +dialog, title:(Progress|Save File|Save As)"
+      # "tag +dialog, title:(Open|Progress|Save File|Save As)"
+      "tag +dialog, class:(xdg-desktop-portal-gtk)"
+      "float, tag:dialog"
+      "center, tag:dialog"
+      "pin, tag:dialog"
+      "noborder, tag:dialog"
     ];
   };
 }
