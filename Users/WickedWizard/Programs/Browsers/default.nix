d61e03645f5c59283908238ae7401c0d87f9caab
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-12-07 18:53:14 +0530

    feat: install junction
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Browsers/default.nix
+++ Users/WickedWizard/Programs/Browsers/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./chromium.nix
     ./firefox.nix
+    ./junction.nix
     ./librewolf.nix
     ./tor.nix
   ];
---
+++ Users/WickedWizard/Programs/Browsers/junction.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [ junction ];
+
+  xdg.mimeApps.defaultApplications = {
+    "default-web-browser" = [ "re.sonny.Junction.desktop" ];
+    "text/html" = [ "re.sonny.Junction.desktop" ];
+    "x-scheme-handler/http" = [ "re.sonny.Junction.desktop" ];
+    "x-scheme-handler/https" = [ "re.sonny.Junction.desktop" ];
+    "x-scheme-handler/about" = [ "re.sonny.Junction.desktop" ];
+    "x-scheme-handler/unknown" = [ "re.sonny.Junction.desktop" ];
+  };
+
+  wayland.windowManager.hyprland.settings.windowrulev2 = [
+    "tag +dialog, class:(re.sonny.Junction)"
+  ];
+}
