2d24d4b38f4c8e21f674c87c2f764d05f26fe6ff
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2024-12-08 20:38:34 +0530

    feat: rename specialisation & enable kde
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Packages/desktop-specialisation.nix
+++ System/HP-Laptop/Packages/desktop-specialisation.nix
@@ -1,11 +1,14 @@
 { config, lib, ... }:
 let
-  prefer-gnome = true;
+  prefer-gnome = false;
   prefer-kde = !prefer-gnome;
-  cfg = config.specialisation.desktop-environment.configuration;
+  cfg =
+    config.specialisation."desktop-environment-${
+      if prefer-gnome then "gnome" else "kde"
+    }".configuration;
 in
 /**
-  Works with the following logic.
+  Should (or) will work with the following logic.
 
   If Hyprland is enabled (on main), enable KDE in specialisation.
   If GNOME/KDE is enabled, enable Hyprland in specialisation.
@@ -13,8 +16,8 @@ in
   These values must be mkForce'd to avoid issues with pulling in default configs.
 */
 {
-  specialisation.desktop-environment.configuration = {
-    programs.hyprland.enable = lib.mkForce false;
+  specialisation."desktop-environment-${if prefer-gnome then "gnome" else "kde"}".configuration = {
+    programs.hyprland.enable = lib.mkForce (!config.programs.hyprland.enable);
 
     # Enable when either GNOME/KDE is enabled.
     services.xserver.enable = lib.mkForce (
@@ -37,6 +40,7 @@ in
       config.programs.hyprland.enable && prefer-gnome
     );
 
-    services.xserver.displayManager.gdm.enable = lib.mkForce cfg.services.xserver.desktopManager.gnome.enable;
+    services.xserver.displayManager.gdm.enable =
+      lib.mkForce cfg.services.xserver.desktopManager.gnome.enable;
   };
 }
