274650b7fce057ad490668964742b7da19f34632
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-02-08 20:34:29 +0530

    feat: switch from systemd-resolved to stubby
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/Common/Services/networking.nix
+++ System/Common/Services/networking.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   networking = {
     firewall = {
@@ -6,27 +7,58 @@
     };
 
     nameservers = [
-      "1.1.1.1#cloudflare-dns.com"
-      "1.0.0.1#cloudflare-dns.com"
-      "2606:4700:4700::1111#cloudflare-dns.com"
-      "2606:4700:4700::1001#cloudflare-dns.com"
+      "127.0.0.1"
+      "::1"
     ];
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      dns = "none";
+    };
   };
 
-  services.resolved = {
+  environment.systemPackages = [
+    (pkgs.writeScriptBin "stubby-hash-generator" "echo | ${pkgs.openssl}/bin/openssl s_client -connect $1 2>/dev/null | ${pkgs.openssl}/bin/openssl x509 -pubkey -noout | ${pkgs.openssl}/bin/openssl pkey -pubin -outform der | ${pkgs.openssl}/bin/openssl dgst -sha256 -binary | ${pkgs.openssl}/bin/openssl enc -base64")
+  ];
+
+  services.stubby = {
     enable = true;
-    dnssec = "false";
-    llmnr = "true";
-    domains = [ "~." ];
-    dnsovertls = "true";
-    fallbackDns = [
-      "8.8.8.8"
-      "2001:4860:4860::8844"
-    ];
-    extraConfig = ''
-      MulticastDNS=resolve
-      DNSStubListenerExtra=0.0.0.0
-    '';
+    settings = pkgs.stubby.passthru.settingsExample // {
+      tls_min_version = "GETDNS_TLS1_3";
+      dnssec = "GETDNS_EXTENSION_TRUE";
+      upstream_recursive_servers = [
+        {
+          address_data = "1.1.1.1";
+          tls_auth_name = "cloudflare-dns.com";
+        }
+        {
+          address_data = "1.0.0.1";
+          tls_auth_name = "cloudflare-dns.com";
+        }
+        {
+          address_data = "2606:4700:4700::1111";
+          tls_auth_name = "cloudflare-dns.com";
+        }
+        {
+          address_data = "2606:4700:4700::1001";
+          tls_auth_name = "cloudflare-dns.com";
+        }
+        {
+          address_data = "9.9.9.9";
+          tls_auth_name = "dns.quad9.net";
+        }
+        {
+          address_data = "149.112.112.112";
+          tls_auth_name = "dns.quad9.net";
+        }
+        {
+          address_data = "2620:fe::fe";
+          tls_auth_name = "dns.quad9.net";
+        }
+        {
+          address_data = "2620:fe::9";
+          tls_auth_name = "dns.quad9.net";
+        }
+      ];
+    };
   };
 }
