dfd640dcf79ef5204e4965b1bcffffe832c7ddfc
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-02-10 08:20:09 +0530

    fix: remove fix for 'nix-env -e' hm
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/home.nix
+++ Users/WickedWizard/home.nix
@@ -10,7 +10,4 @@
   xdg.mimeApps.enable = true;
   programs.home-manager.enable = true;
   programs.autostart.enable = true;
-
-  # Fixes nix-community/home-manager#5552
-  xdg.configFile."systemd/user/.hm-keep".text = "";
 }
