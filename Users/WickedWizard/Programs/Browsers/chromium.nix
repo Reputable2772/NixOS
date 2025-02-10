a7ba26df336ee87ca600c8faa8ee5f83f4602a91
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-02-10 08:19:11 +0530

    feat: convert chromium to flatpak
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Browsers/chromium.nix
+++ Users/WickedWizard/Programs/Browsers/chromium.nix
@@ -1,13 +1,21 @@
 {
   config',
-  pkgs,
   lib,
   ...
 }:
 {
-  home.packages = with pkgs; [ ungoogled-chromium ];
+  services.flatpak = {
+    packages = [ "io.github.ungoogled_software.ungoogled_chromium" ];
+    overrides."io.github.ungoogled_software.ungoogled_chromium" =
+      lib.optionalAttrs (config'.dir ? browsers && config'.dir.browsers != null)
+        {
+          Context.filesystems = "${config'.dir.browsers}/Chromium";
+          Environment.CHROME_USER_DATA_DIR = "${config'.dir.browsers}/Chromium";
+        };
+  };
 
-  home.sessionVariables.CHROME_USER_DATA_DIR = lib.mkIf (
-    config'.dir ? browsers
-  ) "${config'.dir.browsers}/Chromium";
+  # home.packages = with pkgs; [ ungoogled-chromium ];
+  # home.sessionVariables.CHROME_USER_DATA_DIR = lib.mkIf (
+  #   config'.dir ? browsers
+  # ) "${config'.dir.browsers}/Chromium";
 }
