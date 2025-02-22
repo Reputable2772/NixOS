a157f9fee83653ea7ae223795c83e6019f32d15d
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-02-22 22:48:58 +0530

    feat: remove catppuccin and change to dark theme - gtk, qt
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Desktop/Theming/icons.nix
+++ Users/WickedWizard/Programs/Desktop/Theming/icons.nix
@@ -1,9 +1,4 @@
-{
-  osConfig,
-  pkgs,
-  lib,
-  ...
-}:
+{ pkgs, ... }:
 {
   gtk = {
     enable = true;
@@ -12,10 +7,4 @@
       package = pkgs.adwaita-icon-theme;
     };
   };
-
-  qt.platformTheme.name = lib.mkIf (!osConfig.services.desktopManager.plasma6.enable) "qtct";
-
-  # Icon theme is only for qt apps, and to be configured manually using
-  # qt5ct & qt6ct.
-  home.packages = with pkgs; [ papirus-icon-theme ];
 }
--- Users/WickedWizard/Programs/Desktop/Theming/themes.nix
+++ Users/WickedWizard/Programs/Desktop/Theming/themes.nix
@@ -1,56 +1,26 @@
+{ pkgs, ... }:
 {
-  osConfig,
-  pkgs,
-  lib,
-  ...
-}:
-let
-  # Also known as flavor
-  variant = "mocha";
-  accent = "blue";
+  home.sessionVariables.QT_QPA_PLATFORMTHEME = "qt5ct";
 
-  # Only for Catppuccin GTK
-  tweaks = [ ];
-  size = "standard";
+  home.packages = with pkgs; [
+    # Set theme here. It is not declarative.
+    libsForQt5.qt5ct
+    kdePackages.qt6ct
 
-  inherit (lib.strings) concatStringsSep optionalString;
+    # kdePackages.breeze
+    libsForQt5.breeze-qt5
+  ];
 
-  qt_theme = pkgs.catppuccin-kvantum.override {
-    inherit variant accent;
-  };
-  gtk_theme = pkgs.catppuccin-gtk.override {
-    inherit size tweaks;
-    accents = [ accent ];
-    variant = variant;
-  };
-in
-{
-  qt = {
-    enable = true;
-    # This needs to be set using Application Styles in KDE System Settings, to avoid segfaults with Plasma Shell.
-    style.name = lib.mkIf (!osConfig.services.desktopManager.plasma6.enable) "kvantum";
-  };
-
+  # GTK4 applications
   dconf.settings."org/gnome/desktop/interface" = {
     color-scheme = "prefer-dark";
-    font-antialiasing = "grayscale";
-    font-hinting = "slight";
   };
 
-  gtk.theme = {
-    name =
-      "catppuccin-${variant}-${accent}-${size}"
-      + optionalString (tweaks != [ ]) ("+" + concatStringsSep "," tweaks);
-    package = gtk_theme;
-  };
-
-  xdg.configFile = lib.mkIf (!osConfig.services.desktopManager.plasma6.enable) {
-    # Set theme name
-    "Kvantum/kvantum.kvconfig".text = ''
-      theme=catppuccin-${variant}-${accent}
-    '';
-    # Symlink theme directory
-    "Kvantum/catppuccin-${variant}-${accent}".source =
-      "${qt_theme}/share/Kvantum/catppuccin-${variant}-${accent}";
+  # GTK3 applications
+  gtk = {
+    enable = true;
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
   };
 }
