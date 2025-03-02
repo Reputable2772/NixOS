0c2f4a29798fd4bb703bca05b47b87edf01ccd0f
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-03-02 12:23:25 +0530

    feat: switch to kde
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Packages/kde.nix
+++ System/HP-Laptop/Packages/kde.nix
@@ -5,8 +5,8 @@
   ...
 }:
 {
-  services.xserver.enable = lib.mkDefault false;
-  services.desktopManager.plasma6.enable = false;
+  services.xserver.enable = lib.mkDefault true;
+  services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     elisa
     discover
