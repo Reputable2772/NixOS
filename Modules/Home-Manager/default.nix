983c0febe70d8100412b312397f091cbef001861
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-03-07 20:43:43 +0530

    fix: remove flatpak-config module
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Modules/Home-Manager/default.nix
+++ Modules/Home-Manager/default.nix
@@ -1,15 +1,8 @@
-{ inputs, ... }:
 {
   imports = [
     ./autostart.nix
     ./backup.nix
-    ./flatpak-config.nix
     ./quadlets.nix
     ./shells.nix
-    ./files.nix
-  ];
-
-  disabledModules = [
-    "${inputs.home-manager}/modules/files.nix"
   ];
 }
