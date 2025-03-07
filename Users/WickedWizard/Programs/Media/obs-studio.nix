c063f2b724f1aa842ec705983782c664e44f6ad5
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-03-07 19:45:33 +0530

    feat: add obs-vkcapture plugin
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Media/obs-studio.nix
+++ Users/WickedWizard/Programs/Media/obs-studio.nix
@@ -4,8 +4,9 @@
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
       obs-pipewire-audio-capture
+      obs-vkcapture
     ];
   };
 
-  # xdg.configFile."obs-studio/themes/".source = "${sources.obs_catppuccin.src}/themes";
+  xdg.configFile."obs-studio/themes/".source = "${sources.obs_catppuccin.src}/themes";
 }
