15da0bbdf9cb2b8127d8b3d928b50b16cef81954
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-03-08 18:54:21 +0530

    feat: add uwsm wrapper around hyprland
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Packages/hyprland.nix
+++ System/HP-Laptop/Packages/hyprland.nix
@@ -1,8 +1,21 @@
 {
-  # TODO: Wrap Hyprland package with UWSM so that the command 'Hyprland' starts uwsm first.
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
     withUWSM = true;
   };
+
+  environment.systemPackages = [
+    (pkgs.writeScriptBin "hyprland-start" ''
+      if ${lib.getExe config.programs.uwsm.package} check may-start; then
+        exec ${lib.getExe config.programs.uwsm.package} start hyprland-uwsm.desktop
+      fi
+    '')
+  ];
 }
