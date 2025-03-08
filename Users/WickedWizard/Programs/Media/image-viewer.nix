929991718dd25c04230ec3f4e9cd85daedce16ac
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-03-08 14:12:33 +0530

    fix: image viewer use loupe flatpak and gwenview native
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Media/image-viewer.nix
+++ Users/WickedWizard/Programs/Media/image-viewer.nix
@@ -1,47 +1,11 @@
 {
-  osConfig,
+  config,
   pkgs,
+  lib,
   ...
 }:
 {
-  home.packages = with pkgs; [
-    (if osConfig.services.desktopManager.plasma6.enable then kdePackages.gwenview else loupe)
-  ];
+  services.flatpak.packages = lib.optional (!config.programs.plasma.enable) "org.gnome.Loupe";
 
-  xdg.mimeApps.defaultApplications =
-    if osConfig.services.desktopManager.plasma6.enable then
-      {
-        "image/avif" = "org.kde.gwenview.desktop";
-        "image/bmp" = "org.kde.gwenview.desktop";
-        "image/heif" = "org.kde.gwenview.desktop";
-        "image/jpeg" = "org.kde.gwenview.desktop";
-        "image/png" = "org.kde.gwenview.desktop";
-        "image/webp" = "org.kde.gwenview.desktop";
-        "image/x-icns" = "org.kde.gwenview.desktop";
-      }
-    else
-      {
-        "image/jpeg" = "org.gnome.Loupe.desktop";
-        "image/png" = "org.gnome.Loupe.desktop";
-        "image/gif" = "org.gnome.Loupe.desktop";
-        "image/webp" = "org.gnome.Loupe.desktop";
-        "image/tiff" = "org.gnome.Loupe.desktop";
-        "image/x-tga" = "org.gnome.Loupe.desktop";
-        "image/vnd-ms.dds" = "org.gnome.Loupe.desktop";
-        "image/x-dds" = "org.gnome.Loupe.desktop";
-        "image/bmp" = "org.gnome.Loupe.desktop";
-        "image/vnd.microsoft.icon" = "org.gnome.Loupe.desktop";
-        "image/vnd.radiance" = "org.gnome.Loupe.desktop";
-        "image/x-exr" = "org.gnome.Loupe.desktop";
-        "image/x-portable-bitmap" = "org.gnome.Loupe.desktop";
-        "image/x-portable-graymap" = "org.gnome.Loupe.desktop";
-        "image/x-portable-pixmap" = "org.gnome.Loupe.desktop";
-        "image/x-portable-anymap" = "org.gnome.Loupe.desktop";
-        "image/x-qoi" = "org.gnome.Loupe.desktop";
-        "image/svg+xml" = "org.gnome.Loupe.desktop";
-        "image/svg+xml-compressed" = "org.gnome.Loupe.desktop";
-        "image/avif" = "org.gnome.Loupe.desktop";
-        "image/heic" = "org.gnome.Loupe.desktop";
-        "image/jxl" = "org.gnome.Loupe.desktop";
-      };
+  home.packages = lib.optional config.programs.plasma.enable pkgs.kdePackages.gwenview;
 }
