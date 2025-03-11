df309156e2606c5e33ddb1ab4b978e1c70d26ec5
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-03-11 22:17:12 +0530

    feat: add restart-all-podman-units option to quadlets
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Modules/Home-Manager/quadlets.nix
+++ Modules/Home-Manager/quadlets.nix
@@ -10,15 +10,57 @@
 let
   inherit (lib) types;
   inherit (lib.attrsets)
+    attrNames
     hasAttrByPath
     mapAttrsToList
     optionalAttrs
     recursiveUpdate
     ;
+  inherit (lib.lists) optional;
   inherit (lib.options) mkEnableOption mkOption;
-  inherit (lib.strings) concatMapStringsSep concatStringsSep;
+  inherit (lib.strings) concatMapStringsSep concatStringsSep replaceStrings;
 
   cfg = config.programs.quadlets;
+
+  contents =
+    [
+      "mkdir -p $out $out/source $out/units"
+    ]
+    ++ (mapAttrsToList (
+      n: v:
+      let
+        # Don't override if a user already set ExecStartPre. Accomplished by recursiveUpdate
+        val = recursiveUpdate (optionalAttrs
+          (
+            cfg.mkdir
+            && hasAttrByPath [
+              "Container"
+              "Volume"
+            ] v
+            && (v.Container.Volume != null || v.Container.Volume != [ ])
+          )
+          {
+            Service.ExecStartPre = [
+              (pkgs.writeShellScript "${n}-mkdir" (
+                concatMapStringsSep "\n" (
+                  x: "${pkgs.coreutils}/bin/mkdir -p ${builtins.elemAt (builtins.split ":" x) 0}"
+                ) v.Container.Volume
+              ))
+            ];
+          }
+        ) v;
+        file = pkgs.writeTextDir n (lib'.toSystemdUnit val);
+      in
+      ''
+        cp ${file}/* $out/source
+        QUADLET_UNIT_DIRS=${file} ${osConfig.virtualisation.podman.package}/libexec/podman/quadlet -user $out/units
+
+        for file in $(find $out/units -type f -exec realpath --relative-to $out/units {} \;); do
+          substituteInPlace $out/units/$file \
+            --replace-warn ${file}/\$\{XDG_RUNTIME_DIR} \$\{XDG_RUNTIME_DIR}
+        done
+      ''
+    ) cfg.quadlets);
 in
 {
   options.programs.quadlets = {
@@ -42,54 +84,23 @@ in
         };
       description = "All the quadlets that need to have systemd unit files generated";
     };
+    restart-all-units = mkEnableOption "creating a script that restarts all units" // {
+      default = true;
+    };
   };
 
-  config =
-    let
-      contents =
-        [
-          "mkdir -p $out $out/source $out/units"
-        ]
-        ++ (mapAttrsToList (
-          n: v:
-          let
-            # Don't override if a user already set ExecStartPre. Accomplished by recursiveUpdate
-            val = recursiveUpdate (optionalAttrs
-              (
-                cfg.mkdir
-                && hasAttrByPath [
-                  "Container"
-                  "Volume"
-                ] v
-                && (v.Container.Volume != null || v.Container.Volume != [ ])
-              )
-              {
-                Service.ExecStartPre = [
-                  (pkgs.writeShellScript "${n}-mkdir" (
-                    concatMapStringsSep "\n" (
-                      x: "${pkgs.coreutils}/bin/mkdir -p ${builtins.elemAt (builtins.split ":" x) 0}"
-                    ) v.Container.Volume
-                  ))
-                ];
-              }
-            ) v;
-            file = pkgs.writeTextDir n (lib'.toSystemdUnit val);
-          in
-          ''
-            cp ${file}/* $out/source
-            QUADLET_UNIT_DIRS=${file} ${osConfig.virtualisation.podman.package}/libexec/podman/quadlet -user $out/units
-
-            for file in $(find $out/units -type f -exec realpath --relative-to $out/units {} \;); do
-              substituteInPlace $out/units/$file \
-                --replace-warn ${file}/\$\{XDG_RUNTIME_DIR} \$\{XDG_RUNTIME_DIR}
-            done
-          ''
-        ) cfg.quadlets);
-    in
-    lib.mkIf cfg.enable {
-      xdg.configFile."systemd/user/" = {
-        source = "${pkgs.runCommand "quadlet-generator" { } (concatStringsSep "\n" contents)}/units";
-        recursive = true;
-      };
+  config = lib.mkIf cfg.enable {
+    xdg.configFile."systemd/user/" = {
+      source = "${pkgs.runCommand "quadlet-generator" { } (concatStringsSep "\n" contents)}/units";
+      recursive = true;
     };
+
+    home.packages = optional cfg.restart-all-units (
+      pkgs.writeShellScriptBin "restart-all-podman-units" "systemctl --user restart ${
+        concatMapStringsSep " " (
+          x: replaceStrings [ ".container" ".network" ] [ ".service" "-network.service" ] x
+        ) (attrNames cfg.quadlets)
+      }"
+    );
+  };
 }
