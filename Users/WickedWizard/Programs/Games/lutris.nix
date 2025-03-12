5af4f8d77016f1b41570c8171d53319c73163c2f
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-03-12 17:44:36 +0530

    fix: maintenance; remove extra code lutris
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Games/lutris.nix
+++ Users/WickedWizard/Programs/Games/lutris.nix
@@ -1,10 +1,7 @@
-{ osConfig, pkgs, ... }:
+{ pkgs, ... }:
 {
   home.packages = with pkgs; [
     (lutris.override {
-      extraLibraries = pkgs: osConfig.programs.nix-ld.libraries;
-      # Fixes NixOS/nixpkgs#285748
-      extraPkgs = pkgz: with pkgz; [ winetricks ];
       # Since steam is a flatpak.
       steamSupport = false;
     })
