ed2afc0bfafb3ac84bb626bc502986d8476f69f8
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-03-29 18:45:20 +0530

    feat: add lanzaboote to substituters
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/Hardware/lanzaboote.nix
+++ System/HP-Laptop/Hardware/lanzaboote.nix
@@ -10,6 +10,11 @@
     inputs.lanzaboote.nixosModules.lanzaboote
   ];
 
+  nix.settings = {
+    substituters = [ "https://lanzaboote.cachix.org/" ];
+    trusted-public-keys = [ "lanzaboote.cachix.org-1:Nt9//zGmqkg1k5iu+B3bkj3OmHKjSw9pvf3faffLLNk=" ];
+  };
+
   boot.loader.systemd-boot.enable = lib.mkForce (!config.boot.lanzaboote.enable);
   boot.lanzaboote = {
     enable = true;
