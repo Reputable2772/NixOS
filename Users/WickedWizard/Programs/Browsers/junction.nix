1f4d0e34e475d8023dfdbb23a2ba97969918a106
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-05-18 09:58:32 +0530

    feat: passthrough XDG_DATA_DIRS env and folder perms for junction
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Browsers/junction.nix
+++ Users/WickedWizard/Programs/Browsers/junction.nix
@@ -1,5 +1,19 @@
+{ pkgs, ... }:
 {
   services.flatpak.packages = [ "re.sonny.Junction" ];
+
+  systemd.user.services."flatpak-managed-install".Service = {
+    ExecStartPost = [
+      (pkgs.writeShellScript "flatpak-junction-overrides" ''
+        # Allow XDG_DATA_DIRS entirely, with sandbox overrides from https://github.com/flatpak/flatpak/blob/main/common/flatpak-run.c#L552
+
+        ${pkgs.flatpak}/bin/flatpak --user override --env=XDG_DATA_DIRS="/app/share:/usr/share:$XDG_DATA_DIRS" re.sonny.Junction
+        for path in $(echo $XDG_DATA_DIRS | ${pkgs.coreutils-full}/bin/tr ':' '\n'); do
+          ${pkgs.flatpak}/bin/flatpak --user override --filesystem="$path" re.sonny.Junction
+        done
+      '')
+    ];
+  };
 
   xdg.mimeApps.defaultApplications = {
     "default-web-browser" = [ "re.sonny.Junction.desktop" ];
