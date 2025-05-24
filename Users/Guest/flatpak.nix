e4ce8b62e260a2d96a5436ffd54d2c2411264448
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-05-24 20:46:43 +0530

    fix: remove flatpak for guest user
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/Guest/flatpak.nix
+++ Users/Guest/flatpak.nix
@@ -2,17 +2,12 @@
 {
   services.flatpak = {
     enable = true;
-    update.onActivation = true;
     uninstallUnmanaged = true;
     packages = [ "com.github.tchx84.Flatseal" ];
     remotes = [
       {
         name = "flathub";
         location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-      }
-      {
-        name = "flathub-beta";
-        location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
       }
     ];
   };
