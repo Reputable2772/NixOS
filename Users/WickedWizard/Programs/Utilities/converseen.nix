77819b59748743044fef16704436228ff8128ab0
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-07-06 14:30:22 +0530

    feat: install converseen and devtoolbox
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

---
+++ Users/WickedWizard/Programs/Utilities/converseen.nix
@@ -0,0 +1,3 @@
+{
+  services.flatpak.packages = [ "net.fasterland.converseen" ];
+}
--- Users/WickedWizard/Programs/Utilities/default.nix
+++ Users/WickedWizard/Programs/Utilities/default.nix
@@ -3,6 +3,8 @@
     ./ark.nix
     ./calculator.nix
     ./clock.nix
+    ./converseen.nix
+    ./devtoolbox.nix
     ./evince.nix
     ./fontconfig.nix
     ./foot.nix
