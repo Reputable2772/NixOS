31f8e75538b409e71b29a633b6876f7ab70ee042
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-10-06 14:50:48 +0530

    fix: rename power-graph to power-usage and change scale
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Shell/power-usage.nix
+++ Users/WickedWizard/Programs/Shell/power-usage.nix
@@ -8,9 +8,9 @@
   home.packages = with pkgs; [
     ttyplot
     osConfig.boot.kernelPackages.turbostat
-    (writeShellScriptBin "power-graph" ''
+    (writeShellScriptBin "power-usage" ''
       sudo test
-      sudo ${osConfig.boot.kernelPackages.turbostat}/bin/turbostat --Summary --quiet --show PkgWatt --interval 1 | ${lib.getExe gawk} '{ printf("%.2f\n" , $1); fflush(); }' | ${ttyplot} -s 100 -t "Turbostat - CPU Power (watts)" -u "watts"
+      sudo ${osConfig.boot.kernelPackages.turbostat}/bin/turbostat --Summary --quiet --show PkgWatt --interval 1 | ${lib.getExe gawk} '{ printf("%.2f\n" , $1); fflush(); }' | ${lib.getExe ttyplot} -s 30 -t "Turbostat - CPU Power (watts)" -u "watts"
     '')
   ];
 }
