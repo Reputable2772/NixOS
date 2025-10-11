31fedd2c6de74b8a82af81c3fc5c45a5401c7a63
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-10-11 17:12:38 +0530

    feat: add deepMerge library function
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- lib/default.nix
+++ lib/default.nix
@@ -6,6 +6,7 @@ in
   absoluteToRelativePath = import ./absoluteToRelativePath.nix { inherit lib; };
   recurseDirectory = import ./recurseDirectory.nix { inherit lib; };
   toSystemdUnit = import ./toSystemdUnit.nix { inherit lib; };
+  deepMerge = import ./deepMerge.nix;
 
   # This is a derivation, rather than a function
   iniToNix = import ./iniToNix.nix { inherit pkgs; };
