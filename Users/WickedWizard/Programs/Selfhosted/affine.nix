889d05b3d3936ebab53d828866af961406cdbf1b
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-10-12 22:19:51 +0530

    feat: autostart AFFiNE
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Selfhosted/affine.nix
+++ Users/WickedWizard/Programs/Selfhosted/affine.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [ affine ];
+  home.packages = with pkgs; [ affine-bin ];
+  programs.autostart.packages = with pkgs; [ affine-bin ];
 
   system-config.nixpkgs.config.permittedInsecurePackages = [ "electron-35.7.5" ];
 }
