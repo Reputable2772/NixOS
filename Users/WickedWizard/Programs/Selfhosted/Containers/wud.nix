1cf051b00b46f66c2f78af5f7ac2b6967cde7a1e
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-02-08 13:56:26 +0530

    feat: wud uses podman-socket-proxy
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Selfhosted/Containers/wud.nix
+++ Users/WickedWizard/Programs/Selfhosted/Containers/wud.nix
@@ -9,14 +9,32 @@ let
 in
 {
   containers.caddy.services.wud = "wud:3000";
+  containers.podman-socket-proxy.allowedMethods = {
+    CONTAINERS = 1;
+    ALLOW_START = 1;
+    ALLOW_STOP = 1;
+    IMAGES = 1;
+    POST = 0;
+  };
 
-  programs.quadlets.quadlets."wud.container" = lib.attrsets.recursiveUpdate {
-    Container = {
-      Image = "ghcr.io/getwud/wud:main";
-      Volume = [
-        "/run/user/1000/podman/podman.sock:/var/run/docker.sock"
-      ];
-    }
-    // utils.appendEnv "wud";
-  } (utils.containerDefaults "wud" "systemd-caddy");
+  programs.quadlets.quadlets."wud.container" =
+    lib.attrsets.recursiveUpdate (utils.containerDefaults "wud" "systemd-caddy")
+      {
+        Container = utils.appendEnv "wud" // {
+          Image = "ghcr.io/getwud/wud:main";
+          Network = [
+            "systemd-caddy"
+            "podman-socket"
+          ];
+          Environment = [
+            "WUD_WATCHER_DOCKER_HOST=podman-socket-proxy"
+            "WUD_WATCHER_DOCKER_PORT=2375"
+            "WUD_WATCHER_DOCKER_SOCKET=tcp://podman-socket-proxy"
+            "WUD_WATCHER_DOCKER_WATCHEVENTS=true"
+            "WUD_WATCHER_DOCKER_WATCHATSTART=true"
+            "WUD_WATCHER_DOCKER_WATCHBYDEFAULT=true"
+            "WUD_WATCHER_DOCKER_WATCHALL=true"
+          ];
+        };
+      };
 }
