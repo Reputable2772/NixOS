9be07425e1cd55ec83d18a1c998e5171d5939f16
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-02-27 15:30:42 +0530

    feat: move linkding from local to podman volume
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Selfhosted/Containers/linkding.nix
+++ Users/WickedWizard/Programs/Selfhosted/Containers/linkding.nix
@@ -1,13 +1,30 @@
 {
   containers.caddy.services.linkding = "linkding:9090";
 
+  programs.quadlets.quadlets."linkding.volume" = {
+    __options = {
+      mkdir = false;
+      appendEnv = false;
+      appendEnvFiles = false;
+      unitDefaults = true;
+      addNetworkDependency = false;
+      mapVolumes = false;
+    };
+    Unit.Description = "Linkding data Volume";
+    Volume.VolumeName = "linkding";
+    Service = {
+      Type = "oneshot";
+      Restart = "on-failure";
+    };
+  };
+
   programs.quadlets.quadlets."linkding.container" = {
     Container = {
       ContainerName = "linkding";
       Network = "systemd-caddy";
       Image = "docker.io/sissbruecker/linkding:latest";
       Volume = [
-        "data:/etc/linkding/data"
+        "linkding.volume:/etc/linkding/data"
       ];
     };
   };
