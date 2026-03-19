d4b936d81568df57d774b5f89be2db1985df802e
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-03-19 21:52:53 +0530

    feat: remove unused container files
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Selfhosted/Containers/default.nix
+++ Users/WickedWizard/Programs/Selfhosted/Containers/default.nix
@@ -1,18 +1,13 @@
 {
   imports = [
-    # ./affine.nix
     ./baikal.nix
     ./caddy.nix
     ./caddy-network.nix
     ./ente.nix
-    # ./gluetun.nix
-    # ./lidarr.nix
     ./linkding.nix
     ./n8n.nix
-    # ./navidrome.nix
     ./ollama.nix
     ./podman-socket-proxy.nix
-    # ./qbittorrent.nix
     ./secrets-loader.nix
     ./syncthing.nix
     ./vaultwarden.nix
