abaefc60cd1b78225f9e9d098f1d148bd1e8567e
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-05-06 00:24:31 +0530

    feat: cosmic use nixpkgs module
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/Lenovo-Laptop/Packages/cosmic.nix
+++ System/Lenovo-Laptop/Packages/cosmic.nix
@@ -1,18 +1,6 @@
-{ inputs, lib, ... }:
-let
-  # Check if nixpkgs.follows = "nixos-cosmic/nixpkgs"
-  cosmic-enabled = inputs.nixpkgs == inputs.nixos-cosmic.inputs.nixpkgs;
-in
-lib.optionalAttrs cosmic-enabled {
-  nix.settings = {
-    substituters = [ "https://cosmic.cachix.org?priority=50" ];
-    trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-  };
-  imports = [
-    inputs.nixos-cosmic.nixosModules.default
-  ];
-
-  # Uncomment `nixpkgs.follows` line in flake.nix to enable cosmic.
-  services.desktopManager.cosmic.enable = true;
-  services.displayManager.cosmic-greeter.enable = true;
+{ lib, ... }:
+{
+  services.xserver.enable = lib.mkDefault false;
+  services.desktopManager.cosmic.enable = false;
+  services.displayManager.cosmic-greeter.enable = false;
 }
