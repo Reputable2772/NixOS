3a84cbbb77cb2ace3aef197d3b3b57c8bd9ebcda
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-06-22 20:16:38 +0530

    feat: use bitwarden flatpak instead of nix package
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Selfhosted/bitwarden.nix
+++ Users/WickedWizard/Programs/Selfhosted/bitwarden.nix
@@ -1,29 +1,32 @@
 {
   config,
-  pkgs,
+  # pkgs,
   lib,
   ...
 }:
 {
   # Bitwarden itself can act as a ssh agent,
   # making it infinitely more useful.
-  home.packages = with pkgs; [
-    bitwarden-desktop
-    # https://socket.dev/blog/bitwarden-cli-compromised
-    # bitwarden-cli
-  ];
+  # home.packages = with pkgs; [
+  # bitwarden-desktop
+  # https://socket.dev/blog/bitwarden-cli-compromised
+  # bitwarden-cli
+  # ];
 
-  system-config.nixpkgs.config.permittedInsecurePackages = [ "electron-39.8.10" ];
+  # system-config.nixpkgs.config.permittedInsecurePackages = [ "electron-39.8.10" ];
 
-  system-config.environment.etc."polkit-1/actions/com.bitwarden.Bitwarden.policy".source =
-    "${pkgs.bitwarden-desktop}/share/polkit-1/actions/com.bitwarden.Bitwarden.policy";
+  # system-config.environment.etc."polkit-1/actions/com.bitwarden.Bitwarden.policy".source =
+  # "${pkgs.bitwarden-desktop}/share/polkit-1/actions/com.bitwarden.Bitwarden.policy";
 
   wayland.windowManager.hyprland.settings.windowrule = [
     "match:initial_class (Bitwarden), workspace 2"
   ];
 
-  programs.autostart.packages = with pkgs; [ bitwarden-desktop ];
+  # programs.autostart.packages = with pkgs; [ bitwarden-desktop ];
 
   services.ssh-agent.enable = lib.mkForce false;
-  home.sessionVariables.SSH_AUTH_SOCK = "$\{HOME:-${config.home.homeDirectory}}/.bitwarden-ssh-agent.sock";
+  home.sessionVariables.SSH_AUTH_SOCK = "$\{HOME:-${config.home.homeDirectory}}/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock";
+
+  services.flatpak.packages = [ "com.bitwarden.desktop" ];
+  programs.autostart.flatpaks = [ "com.bitwarden.desktop" ];
 }
