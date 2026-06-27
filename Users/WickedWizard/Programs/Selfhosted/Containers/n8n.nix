1c688067f4b40b3f81cc346ebe010454ca13930d
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-06-27 23:20:28 +0530

    fix: n8n - set health-check-server-port for multiple runners
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Selfhosted/Containers/n8n.nix
+++ Users/WickedWizard/Programs/Selfhosted/Containers/n8n.nix
@@ -38,23 +38,71 @@
         "N8N_RUNNERS_TASK_BROKER_URI=http://n8n:5679"
       ];
       Volume = [
+        # Taken from https://github.com/n8n-io/n8n/blob/5f92be5fabfcbd119d364cf4566f679a48acb227/docker/images/runners/n8n-task-runners.json
+        # Don't remove and assume default config. This config is necessary for multiple runners,
+        # since each runner requires its own health-check-server-port and that is not configured
+        # by default.
         "${
           pkgs.writeText "n8n-task-runners" (
             builtins.toJSON {
               task-runners = [
                 {
                   runner-type = "javascript";
+                  workdir = "/home/runner";
+                  command = "/usr/local/bin/node";
+                  args = [
+                    "--disallow-code-generation-from-strings"
+                    "--disable-proto=delete"
+                    "/opt/runners/task-runner-javascript/dist/start.js"
+                  ];
+                  health-check-server-port = "5681";
+                  allowed-env = [
+                    "PATH"
+                    "GENERIC_TIMEZONE"
+                    "NODE_OPTIONS"
+                    "NODE_PATH"
+                    "N8N_RUNNERS_AUTO_SHUTDOWN_TIMEOUT"
+                    "N8N_RUNNERS_TASK_TIMEOUT"
+                    "N8N_RUNNERS_MAX_CONCURRENCY"
+                    "N8N_SENTRY_DSN"
+                    "N8N_VERSION"
+                    "ENVIRONMENT"
+                    "DEPLOYMENT_NAME"
+                    "HOME"
+                  ];
                   env-overrides = {
-                    NODE_FUNCTION_ALLOW_BUILTIN = "fs";
-                    # NODE_FUNCTION_ALLOW_EXTERNAL = "moment,uuid";
+                    NODE_FUNCTION_ALLOW_BUILTIN = "crypto,fs";
+                    # NODE_FUNCTION_ALLOW_EXTERNAL = "moment";
+                    N8N_RUNNERS_HEALTH_CHECK_SERVER_HOST = "0.0.0.0";
                   };
                 }
                 {
                   runner-type = "python";
+                  workdir = "/home/runner";
+                  command = "/opt/runners/task-runner-python/.venv/bin/python";
+                  args = [
+                    "-I"
+                    "-B"
+                    "-X"
+                    "disable_remote_debug"
+                    "-m"
+                    "src.main"
+                  ];
+                  health-check-server-port = "5682";
+                  allowed-env = [
+                    "PATH"
+                    "N8N_RUNNERS_LAUNCHER_LOG_LEVEL"
+                    "N8N_RUNNERS_AUTO_SHUTDOWN_TIMEOUT"
+                    "N8N_RUNNERS_TASK_TIMEOUT"
+                    "N8N_RUNNERS_MAX_CONCURRENCY"
+                    "N8N_SENTRY_DSN"
+                    "N8N_VERSION"
+                    "ENVIRONMENT"
+                    "DEPLOYMENT_NAME"
+                  ];
                   env-overrides = {
-                    PYTHONPATH = "/opt/runners/task-runner-python";
                     N8N_RUNNERS_STDLIB_ALLOW = "json";
-                    # N8N_RUNNERS_EXTERNAL_ALLOW = "numpy,pandas";
+                    # N8N_RUNNERS_EXTERNAL_ALLOW = "";
                   };
                 }
               ];
