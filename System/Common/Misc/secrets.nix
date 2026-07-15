cad0152adcb1ee1444179de6f5b95ca109ec4725
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-07-16 00:40:13 +0530

    feat: centralize agenix secrets loading & move age files to different directory
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/Common/Misc/secrets.nix
+++ System/Common/Misc/secrets.nix
@@ -1,22 +1,94 @@
+# This module is responsible for loading secrets for all users, and home-manager, if it exists.
 {
   inputs,
   config,
   config',
   lib,
+  options,
   ...
 }:
 let
-  inherit (lib.attrsets) attrValues filterAttrs mapAttrs;
+  inherit (lib.attrsets)
+    attrNames
+    attrValues
+    filterAttrs
+    genAttrs
+    mapAttrs
+    mapAttrs'
+    nameValuePair
+    ;
+  inherit (lib.lists) filter intersectLists;
+  inherit (lib.modules) mkMerge;
+  inherit (lib.strings) removeSuffix;
+
+  ageFiles =
+    attrNames config' |> filter (v: config'.${v} ? publicKeys) |> (x: genAttrs x (v: config'.${v}));
+
+  systemPublicKeys =
+    config'.system.${config.networking.hostName}.secrets
+    |> (filterAttrs (n: v: v ? key && v.key != null))
+    |> mapAttrs (n: v: v.key)
+    |> attrValues;
 in
 {
+  options.age = { };
+
   imports = [
     inputs.agenix.nixosModules.default
   ];
 
-  # SSH private keys for the system side.
-  age.identityPaths = lib.pipe config'.system.${config.networking.hostName}.secrets [
-    (filterAttrs (n: v: v ? pkeyfile && v.pkeyfile != null))
-    (mapAttrs (_: v: v.pkeyfile))
-    attrValues
+  config = mkMerge [
+    {
+      # SSH private keys for the system side.
+      age.identityPaths = lib.pipe config'.system.${config.networking.hostName}.secrets [
+        (filterAttrs (n: v: v ? pkeyfile && v.pkeyfile != null))
+        (mapAttrs (_: v: v.pkeyfile))
+        attrValues
+      ];
+
+      age.secrets = lib.pipe ageFiles [
+        (filterAttrs (n: v: (intersectLists systemPublicKeys v.publicKeys) != [ ]))
+        (mapAttrs' (
+          n: v: nameValuePair (removeSuffix ".age" n) { file = ./. + "../../../../Config/Secrets/${n}"; }
+        ))
+      ];
+    }
+    (lib.optionalAttrs (options ? home-manager.sharedModules) {
+      home-manager.sharedModules = [
+        (
+          { config', ... }:
+          {
+            options.age = { };
+
+            imports = [
+              inputs.agenix.homeManagerModules.default
+            ];
+
+            config =
+              let
+                publicKeys = lib.pipe (config'.secrets or { }) [
+                  (filterAttrs (n: v: v ? key && v.key != null))
+                  (mapAttrs (_: v: v.key))
+                  attrValues
+                ];
+              in
+              {
+                age.identityPaths = lib.pipe (config'.secrets or { }) [
+                  (filterAttrs (n: v: v ? pkeyfile && v.pkeyfile != null))
+                  (mapAttrs (_: v: v.pkeyfile))
+                  attrValues
+                ];
+
+                age.secrets = lib.pipe ageFiles [
+                  (filterAttrs (n: v: (intersectLists v.publicKeys publicKeys) != [ ]))
+                  (mapAttrs' (
+                    n: v: nameValuePair (removeSuffix ".age" n) { file = ./. + "../../../../Config/Secrets/${n}"; }
+                  ))
+                ];
+              };
+          }
+        )
+      ];
+    })
   ];
 }
