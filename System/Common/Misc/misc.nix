976556bf91509f24bfc7dd0283e2b435bd27fb50
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-07-15 14:27:35 +0530

    feat: add i18.extraLocalisations
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/Common/Misc/misc.nix
+++ System/Common/Misc/misc.nix
@@ -3,6 +3,18 @@
   time.timeZone = config'.system.timezone;
   i18n.defaultLocale = config'.system.locale;
 
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  };
+
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
