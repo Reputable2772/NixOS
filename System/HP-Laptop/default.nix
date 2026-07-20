85ef16600d83623c925d441728ef518d7e07a167
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-07-20 23:35:13 +0530

    feat: enable lanzaboote for hp-laptop
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/default.nix
+++ System/HP-Laptop/default.nix
@@ -3,6 +3,7 @@
     ./cachix-agent.nix
     ./gnome.nix
     ./hardware-configuration.nix
+    ./lanzaboote.nix
     ./networking.nix
     ./users.nix
   ];
---
+++ System/HP-Laptop/lanzaboote.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    inputs.lanzaboote.nixosModules.lanzaboote
+  ];
+
+  nix.settings = {
+    substituters = [ "https://lanzaboote.cachix.org?priority=40" ];
+    trusted-public-keys = [ "lanzaboote.cachix.org-1:Nt9//zGmqkg1k5iu+B3bkj3OmHKjSw9pvf3faffLLNk=" ];
+  };
+
+  boot.loader.systemd-boot.enable = lib.mkForce (!config.boot.lanzaboote.enable);
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/var/lib/sbctl";
+    settings.editor = true;
+    autoGenerateKeys.enable = true;
+    autoEnrollKeys.enable = true;
+  };
+}
