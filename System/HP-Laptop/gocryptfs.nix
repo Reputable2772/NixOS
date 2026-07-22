6762da13d7be70e8c2bced7b1ddbfeb4fad94841
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-07-23 01:39:38 +0530

    fix: set gocryptfs fusermount path [skip ci]
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/HP-Laptop/gocryptfs.nix
+++ System/HP-Laptop/gocryptfs.nix
@@ -5,7 +5,15 @@
   programs.fuse.userAllowOther = true;
 
   security.pam.mount.enable = true;
-  security.pam.mount.extraVolumes = [
-    "<volume user=\"vishnu\" fstype=\"fuse\" path=\"${pkgs.gocryptfs}/bin/gocryptfs#/home/.vishnu-cipher\" mountpoint=\"/home/vishnu\" options=\"allow_other,quiet,noprealloc\" />"
-  ];
+  security.pam.mount.extraVolumes =
+    let
+      # This wrapper restores the PATH so gocryptfs can find fusermount
+      gocryptfsWrapper = pkgs.writeShellScript "gocryptfs-pam-wrapper" ''
+        export PATH="/run/wrappers/bin:/run/current-system/sw/bin:$PATH"
+        exec ${pkgs.gocryptfs}/bin/gocryptfs "$@"
+      '';
+    in
+    [
+      "<volume user=\"vishnu\" fstype=\"fuse\" path=\"${gocryptfsWrapper}#/home/.vishnu-cipher\" mountpoint=\"/home/vishnu\" options=\"allow_other,quiet,noprealloc\" />"
+    ];
 }
