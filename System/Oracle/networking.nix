58e4d7f98fc977565a4dd47aeecd518598d5fd28
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-08-05 11:34:04 +0530

    feat: allow unprivileged port start = 80 - oracle caddy
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/Oracle/networking.nix
+++ System/Oracle/networking.nix
@@ -1,4 +1,6 @@
 { config, ... }: {
   networking.hostName = "oracle-server";
   networking.firewall = config.hm-config.firewall;
+
+  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
 }
