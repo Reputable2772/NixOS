e922c1d5a651d004b4ed0612d1a01cedd9d0c648
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-08-07 23:33:38 +0530

    fix: tmux - lenovo-laptop - don't build with systemd
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Shell/tmux.nix
+++ Users/WickedWizard/Programs/Shell/tmux.nix
@@ -1,5 +1,6 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   programs.tmux.enable = true;
+  programs.tmux.package = pkgs.tmux.override { withSystemd = false; };
   stylix.targets.tmux.enable = config.programs.tmux.enable;
 }
