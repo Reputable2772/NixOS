52753e5379da5fa7c4110cd9a822f055cf8f238b
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-08-09 10:39:53 +0530

    fix: remove overlay for flatpak glycin issue
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Overlays/default.nix
+++ Overlays/default.nix
@@ -1,12 +1,10 @@
 let
   # rtl8821ce = import ./rtl8821ce.nix;
   affine-bin = import ./affine-bin.nix;
-  flatpak = import ./flatpak.nix;
   prismlauncher = import ./prismlauncher.nix;
 in
 [
   # rtl8821ce
   affine-bin
-  flatpak
   prismlauncher
 ]
