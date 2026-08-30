0020a2762cb3a6970eb684d4b806bf7d0d9dbb45
Reputable2772 <153411261+Reputable2772@users.noreply.github.com>
2026-08-30 13:34:09 +0530

    Revert "chore: update flake.lock & sources (2026-08-29)"

--- _sources/aarch64-linux/generated.nix
+++ _sources/aarch64-linux/generated.nix
@@ -22,14 +22,14 @@
   };
   nix-fast-build = {
     pname = "nix-fast-build";
-    version = "a7217fc192e2cbb3ee6809b1a00604b24ac12152";
+    version = "c65830739d075c82b01470058adc4846e9cb7f62";
     src = fetchFromGitHub {
       owner = "Mic92";
       repo = "nix-fast-build";
-      rev = "a7217fc192e2cbb3ee6809b1a00604b24ac12152";
+      rev = "c65830739d075c82b01470058adc4846e9cb7f62";
       fetchSubmodules = false;
-      sha256 = "sha256-VOzpaf8Si/c7B5xXwxZi+i34LKoDaMgPNZbSMZkpXP4=";
+      sha256 = "sha256-B4AcDLvMqrfBp7k5LgdJC1YjpjPLRNNJBpzy2B9BPcc=";
     };
-    date = "2026-08-28";
+    date = "2026-08-21";
   };
 }
--- _sources/x86_64-linux/generated.nix
+++ _sources/x86_64-linux/generated.nix
@@ -19,15 +19,15 @@
   };
   nix-fast-build = {
     pname = "nix-fast-build";
-    version = "a7217fc192e2cbb3ee6809b1a00604b24ac12152";
+    version = "49143a9692f6a97915e0b4cc6c1cc5b25d09dd27";
     src = fetchFromGitHub {
       owner = "Mic92";
       repo = "nix-fast-build";
-      rev = "a7217fc192e2cbb3ee6809b1a00604b24ac12152";
+      rev = "49143a9692f6a97915e0b4cc6c1cc5b25d09dd27";
       fetchSubmodules = false;
-      sha256 = "sha256-VOzpaf8Si/c7B5xXwxZi+i34LKoDaMgPNZbSMZkpXP4=";
+      sha256 = "sha256-URMu3Gm2fh4lhbzkS0BlERRpK4H0xsiIRFgFY4GqcvM=";
     };
-    date = "2026-08-28";
+    date = "2026-08-14";
   };
   obs_catppuccin = {
     pname = "obs_catppuccin";
