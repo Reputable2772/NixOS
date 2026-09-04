94dd9b1f24cc2ca7b84495adac9871e92b11f386
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-09-04 21:54:10 +0530

    feat: bitwarden - document hibernate issue & allow fingerprint unlock
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- Users/WickedWizard/Programs/Selfhosted/bitwarden.nix
+++ Users/WickedWizard/Programs/Selfhosted/bitwarden.nix
@@ -1,28 +1,43 @@
 {
   config,
-  # pkgs,
+  pkgs,
   lib,
   ...
 }:
 {
-  # Bitwarden itself can act as a ssh agent,
-  # making it infinitely more useful.
-  # home.packages = with pkgs; [
-  # bitwarden-desktop
-  # https://socket.dev/blog/bitwarden-cli-compromised
-  # bitwarden-cli
-  # ];
+  # Bitwarden silently blocks hibernate
+  # Close it before hibernating.
+  # bitwarden/clients#21661
 
-  # system-config.nixpkgs.config.permittedInsecurePackages = [ "electron-39.8.10" ];
+  system-config.environment.etc."polkit-1/actions/com.bitwarden.Bitwarden.policy".source =
+    /**
+      File content taken from here
+      https://github.com/bitwarden/clients/blob/main/apps/desktop/resources/com.bitwarden.desktop.policy
 
-  # system-config.environment.etc."polkit-1/actions/com.bitwarden.Bitwarden.policy".source =
-  # "${pkgs.bitwarden-desktop}/share/polkit-1/actions/com.bitwarden.Bitwarden.policy";
+      Ref: https://bitwarden.com/help/biometrics/
+    */
+    pkgs.writeText "com.bitwarden.Bitwarden.policy" ''
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE policyconfig PUBLIC
+       "-//freedesktop//DTD PolicyKit Policy Configuration 1.0//EN"
+       "http://www.freedesktop.org/standards/PolicyKit/1.0/policyconfig.dtd">
+
+      <policyconfig>
+          <action id="com.bitwarden.Bitwarden.unlock">
+            <description>Unlock Bitwarden</description>
+            <message>Authenticate to unlock Bitwarden</message>
+            <defaults>
+              <allow_any>no</allow_any>
+              <allow_inactive>no</allow_inactive>
+              <allow_active>auth_self</allow_active>
+            </defaults>
+          </action>
+      </policyconfig>
+    '';
 
   wayland.windowManager.hyprland.settings.windowrule = [
     "match:initial_class (Bitwarden), workspace 2"
   ];
-
-  # xdg.autostart.packages = with pkgs; [ bitwarden-desktop ];
 
   services.ssh-agent.enable = lib.mkForce false;
   home.sessionVariables.SSH_AUTH_SOCK = "\${HOME:-${config.home.homeDirectory}}/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock";
