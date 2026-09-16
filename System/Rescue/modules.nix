fda4b500e94c5702342ec9e00dc72bfc5e8f48eb
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-09-16 08:44:43 +0530

    feat: rescue system - ip-update
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- System/Rescue/modules.nix
+++ System/Rescue/modules.nix
@@ -1,6 +1,11 @@
+# This file is generally meant to be used for
+# reverting common config set in Systems/Common.
 { lib, ... }: {
   options.secretspec = lib.mkSinkUndeclaredOptions { };
   options.home-manager = lib.mkSinkUndeclaredOptions { };
 
-  config = { };
+  config = {
+    systemd.services.ip-update = lib.mkForce { };
+    systemd.timers.ip-update = lib.mkForce { };
+  };
 }
